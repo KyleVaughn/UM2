27643dadcc957a47daeb15adeeb659333c13ec58
Kyle Vaughn <kcvaughn@umich.edu>
2021-11-20 16:24:53 -0500

    Tri6 update

--- src/Triangle6_2D.jl
+++ src/Triangle6_2D.jl
@@ -1,5 +1,7 @@
+# @code_warntype checked 2021/11/20
+
 # A quadratic triangle, defined in 2D.
-struct Triangle6_2D{T <: AbstractFloat} <: Face_2D{T}
+struct Triangle6_2D{F <: AbstractFloat} <: Face_2D{F}
     # The points are assumed to be ordered as follows
     # p₁ = vertex A
     # p₂ = vertex B
@@ -7,31 +9,33 @@ struct Triangle6_2D{T <: AbstractFloat} <: Face_2D{T}
     # p₄ = point on the quadratic segment from A to B
     # p₅ = point on the quadratic segment from B to C
     # p₆ = point on the quadratic segment from C to A
-    points::NTuple{6, Point_2D{T}}
+    points::NTuple{6, Point_2D{F}}
 
 end
 
 # Constructors
 # -------------------------------------------------------------------------------------------------
-Triangle6_2D(p₁::Point_2D{T},
-             p₂::Point_2D{T},
-             p₃::Point_2D{T},
-             p₄::Point_2D{T},
-             p₅::Point_2D{T},
-             p₆::Point_2D{T}
-            ) where {T <: AbstractFloat} = Triangle6_2D((p₁, p₂, p₃, p₄, p₅, p₆))
+# @code_warntype checked 2021/11/20
+Triangle6_2D(p₁::Point_2D,
+             p₂::Point_2D,
+             p₃::Point_2D,
+             p₄::Point_2D,
+             p₅::Point_2D,
+             p₆::Point_2D
+            ) = Triangle6_2D((p₁, p₂, p₃, p₄, p₅, p₆))
 
 
 # Methods
 # -------------------------------------------------------------------------------------------------
 # Interpolation
-function (tri6::Triangle6_2D{T})(r::R, s::S) where {T <: AbstractFloat,
+# @code_warntype checked 2021/11/20
+function (tri6::Triangle6_2D{F})(r::R, s::S) where {F <: AbstractFloat,
                                                     R <: Real,
                                                     S <: Real}
-    # See The Visualization Toolkit: An Object-Oriented Approach to 3D Graphics, 4th Edition
+    # See Fhe Visualization Toolkit: An Object-Oriented Approach to 3D Graphics, 4th Edition
     # Chapter 8, Advanced Data Representation, in the interpolation functions section
-    rₜ = T(r)
-    sₜ = T(s)
+    rₜ = F(r)
+    sₜ = F(s)
     return (1 - rₜ - sₜ)*(2(1 - rₜ - sₜ) - 1)*tri6.points[1] +
                                    rₜ*(2rₜ-1)*tri6.points[2] +
                                    sₜ*(2sₜ-1)*tri6.points[3] +
@@ -41,7 +45,8 @@ function (tri6::Triangle6_2D{T})(r::R, s::S) where {T <: AbstractFloat,
 end
 
 # Interpolation using a point instead of (r,s)
-function (tri6::Triangle6_2D{T})(p::Point_2D{T}) where {T <: AbstractFloat}
+# @code_warntype checked 2021/11/20
+function (tri6::Triangle6_2D{F})(p::Point_2D{F}) where {F <: AbstractFloat}
     r = p[1]
     s = p[2]
     return (1 - r - s)*(2(1 - r - s) - 1)*tri6.points[1] +
@@ -52,56 +57,59 @@ function (tri6::Triangle6_2D{T})(p::Point_2D{T}) where {T <: AbstractFloat}
                            4s*(1 - r - s)*tri6.points[6]
 end
 
-function derivative(tri6::Triangle6_2D{T}, r::R, s::S) where {T <: AbstractFloat,
-                                                               R <: Real,
-                                                               S <: Real}
-    # Let T(r,s) be the interpolation function for tri6
-    # Returns ∂T/∂r, ∂T/∂s
-    rₜ = T(r)
-    sₜ = T(s)
-    ∂T_∂r = (4rₜ + 4sₜ - 3)*tri6.points[1] +
+# @code_warntype checked 2021/11/20
+function derivative(tri6::Triangle6_2D{F}, r::R, s::S) where {F <: AbstractFloat,
+                                                              R <: Real,
+                                                              S <: Real}
+    # Let F(r,s) be the interpolation function for tri6
+    # Returns ∂F/∂r, ∂F/∂s
+    rₜ = F(r)
+    sₜ = F(s)
+    ∂F_∂r = (4rₜ + 4sₜ - 3)*tri6.points[1] +
                   (4rₜ - 1)*tri6.points[2] +
             4(1 - 2rₜ - sₜ)*tri6.points[4] +
                       (4sₜ)*tri6.points[5] +
                      (-4sₜ)*tri6.points[6]
 
-    ∂T_∂s = (4rₜ + 4sₜ - 3)*tri6.points[1] +
+    ∂F_∂s = (4rₜ + 4sₜ - 3)*tri6.points[1] +
                   (4sₜ - 1)*tri6.points[3] +
                      (-4rₜ)*tri6.points[4] +
                       (4rₜ)*tri6.points[5] +
             4(1 - rₜ - 2sₜ)*tri6.points[6]
-    return ∂T_∂r, ∂T_∂s
+    return ∂F_∂r, ∂F_∂s
 end
 
-function jacobian(tri6::Triangle6_2D{T}, r::R, s::S) where {T <: AbstractFloat,
-                                                            R <: Real,
-                                                            S <: Real}
+# @code_warntype checked 2021/11/20
+function jacobian(tri6::Triangle6_2D, r::R, s::S) where {R <: Real,
+                                                         S <: Real}
     # Return the 2 x 2 Jacobian matrix
-    ∂T_∂r, ∂T_∂s = derivative(tri6, r, s)
-    return hcat(∂T_∂r.x, ∂T_∂s.x)
+    ∂F_∂r, ∂F_∂s = derivative(tri6, r, s)
+    return hcat(∂F_∂r.x, ∂F_∂s.x)
 end
 
-function area(tri6::Triangle6_2D{T}; N::Int64=12) where {T <: AbstractFloat}
+# @code_warntype checked 2021/11/20
+function area(tri6::Triangle6_2D{F}; N::Int64=12) where {F <: AbstractFloat}
     # Numerical integration required. Gauss-Legendre quadrature over a triangle is used.
-    # Let T(r,s) be the interpolation function for tri6,
+    # Let F(r,s) be the interpolation function for tri6,
     #                             1 1-r                          N
-    # A = ∬ ||∂T/∂r × ∂T/∂s||dA = ∫  ∫ ||∂T/∂r × ∂T/∂s|| ds dr = ∑ wᵢ||∂T/∂r(rᵢ,sᵢ) × ∂T/∂s(rᵢ,sᵢ)||
+    # A = ∬ ||∂F/∂r × ∂F/∂s||dA = ∫  ∫ ||∂F/∂r × ∂F/∂s|| ds dr = ∑ wᵢ||∂F/∂r(rᵢ,sᵢ) × ∂F/∂s(rᵢ,sᵢ)||
     #      D                      0  0                          i=1
     #
     # N is the number of points used in the quadrature.
     # See tuning/Triangle6_2D_area.jl for more info on how N = 12 was chosen.
     w, r, s = gauss_legendre_quadrature(tri6, N)
-    a = T(0)
+    a = F(0)
     for i in 1:N
-        ∂T_∂r, ∂T_∂s = derivative(tri6, r[i], s[i])
-        a += w[i] * abs(∂T_∂r × ∂T_∂s)
+        ∂F_∂r, ∂F_∂s = derivative(tri6, r[i], s[i])
+        a += w[i] * abs(∂F_∂r × ∂F_∂s)
     end
     return a
 end
 
-function triangulate(tri6::Triangle6_2D{T}, N::Int64) where {T <: AbstractFloat}
+# @code_warntype checked 2021/11/20
+function triangulate(tri6::Triangle6_2D{F}, N::Int64) where {F <: AbstractFloat}
     # N is the number of divisions of each edge
-    triangles = Vector{Triangle_2D{T}}(undef, (N+1)*(N+1))
+    triangles = Vector{Triangle_2D{F}}(undef, (N+1)*(N+1))
     if N === 0
         triangles[1] = Triangle_2D(tri6.points[1], tri6.points[2], tri6.points[3])
     else
@@ -126,12 +134,13 @@ function triangulate(tri6::Triangle6_2D{T}, N::Int64) where {T <: AbstractFloat}
     return triangles
 end
 
-function real_to_parametric(p::Point_2D{T}, tri6::Triangle6_2D{T}; N::Int64=30) where {T <: AbstractFloat}
+# @code_warntype checked 2021/11/20
+function real_to_parametric(p::Point_2D{F}, tri6::Triangle6_2D{F}; N::Int64=30) where {F <: AbstractFloat}
     # Convert from real coordinates to the triangle's local parametric coordinates using the
     # the Newton-Raphson method. N is the max number of iterations
     # If a conversion doesn't exist, the minimizer is returned.
-    r = T(1//3) # Initial guess at triangle centroid
-    s = T(1//3)
+    r = F(1//3) # Initial guess at triangle centroid
+    s = F(1//3)
     err₁ = p - tri6(r, s)
     for i = 1:N
         # Inversion is faster for 2 by 2 than \
@@ -147,7 +156,8 @@ function real_to_parametric(p::Point_2D{T}, tri6::Triangle6_2D{T}; N::Int64=30) 
     return Point_2D(r, s)
 end
 
-function in(p::Point_2D{T}, tri6::Triangle6_2D{T}; N::Int64=30) where {T <: AbstractFloat}
+# @code_warntype checked 2021/11/20
+function in(p::Point_2D, tri6::Triangle6_2D; N::Int64=30)
     # Determine if the point is in the triangle using the Newton-Raphson method
     # N is the max number of iterations of the method.
     p_rs = real_to_parametric(p, tri6; N=N)
@@ -164,22 +174,23 @@ function in(p::Point_2D{T}, tri6::Triangle6_2D{T}; N::Int64=30) where {T <: Abst
     end
 end
 
-function intersect(l::LineSegment_2D{T}, tri6::Triangle6_2D{T}) where {T <: AbstractFloat}
+# @code_warntype checked 2021/11/20
+function intersect(l::LineSegment_2D{F}, tri6::Triangle6_2D{F}) where {F <: AbstractFloat}
     # Create the 3 quadratic segments that make up the triangle and intersect each one
     edges = (QuadraticSegment_2D(tri6.points[1], tri6.points[2], tri6.points[4]),
              QuadraticSegment_2D(tri6.points[2], tri6.points[3], tri6.points[5]),
              QuadraticSegment_2D(tri6.points[3], tri6.points[1], tri6.points[6]))
-    intersections = l .∩ edges
-    ipoints = MVector(Point_2D(T, 0),
-                      Point_2D(T, 0),
-                      Point_2D(T, 0),
-                      Point_2D(T, 0),
-                      Point_2D(T, 0),
-                      Point_2D(T, 0)
+    ipoints = MVector(Point_2D(F, 0),
+                      Point_2D(F, 0),
+                      Point_2D(F, 0),
+                      Point_2D(F, 0),
+                      Point_2D(F, 0),
+                      Point_2D(F, 0)
                      )
     n_ipoints = 0x00
     # We need to account for 6 points returned
-    for (npoints, points) in intersections
+    for i = 1:3
+        npoints, points = l ∩ edges[i]
         for i = 1:npoints
             if n_ipoints === 0x00
                 ipoints[1] = points[1]
@@ -204,8 +215,8 @@ function intersect(l::LineSegment_2D{T}, tri6::Triangle6_2D{T}) where {T <: Abst
 end
 intersect(tri6::Triangle6_2D, l::LineSegment_2D) = intersect(l, tri6)
 
-function Base.show(io::IO, tri6::Triangle6_2D{T}) where {T <: AbstractFloat}
-    println(io, "Triangle6_2D{$T}(")
+function Base.show(io::IO, tri6::Triangle6_2D{F}) where {F <: AbstractFloat}
+    println(io, "Triangle6_2D{$F}(")
     for i = 1:6
         p = tri6.points[i]
         println(io, "  $p,")
@@ -215,7 +226,7 @@ end
 
 # Plot
 # -------------------------------------------------------------------------------------------------
-function convert_arguments(P::Type{<:LineSegments}, tri6::Triangle6_2D{T}) where {T <: AbstractFloat}
+function convert_arguments(LS::Type{<:LineSegments}, tri6::Triangle6_2D)
     q₁ = QuadraticSegment_2D(tri6.points[1], tri6.points[2], tri6.points[4])
     q₂ = QuadraticSegment_2D(tri6.points[2], tri6.points[3], tri6.points[5])
     q₃ = QuadraticSegment_2D(tri6.points[3], tri6.points[1], tri6.points[6])
@@ -223,20 +234,19 @@ function convert_arguments(P::Type{<:LineSegments}, tri6::Triangle6_2D{T}) where
     return convert_arguments(P, qsegs)
 end
 
-function convert_arguments(P::Type{<:LineSegments},
-        TA::AbstractArray{<:Triangle6_2D{T}}) where {T <: AbstractFloat}
-    point_sets = [convert_arguments(P, tri6) for tri6 in TA]
-    return convert_arguments(P, reduce(vcat, [pset[1] for pset in point_sets]))
+function convert_arguments(LS::Type{<:LineSegments}, T::Vector{<:Triangle6_2D})
+    point_sets = [convert_arguments(LS, tri6) for tri6 in T]
+    return convert_arguments(LS, reduce(vcat, [pset[1] for pset in point_sets]))
 end
 
-function convert_arguments(P::Type{Mesh{Tuple{Triangle6_2D{T}}}},
-        tri6::Triangle6_2D{T}) where {T <: AbstractFloat}
+function convert_arguments(P::Type{Mesh{Tuple{Triangle6_2D{F}}}},
+        tri6::Triangle6_2D{F}) where {F <: AbstractFloat}
     triangles = triangulate(tri6, 13)
     return convert_arguments(P, triangles)
 end
 
-function convert_arguments(MT::Type{Mesh{Tuple{Vector{Triangle6_2D{T}}}}},
-        AT::Vector{Triangle6_2D{T}}) where {T <: AbstractFloat}
-    triangles = reduce(vcat, triangulate.(AT, 13))
-    return convert_arguments(MT, triangles)
+function convert_arguments(MF::Type{Mesh{Tuple{Vector{Triangle6_2D{F}}}}},
+        AF::Vector{Triangle6_2D{F}}) where {F <: AbstractFloat}
+    triangles = reduce(vcat, triangulate.(AF, 13))
+    return convert_arguments(MF, triangles)
 end
