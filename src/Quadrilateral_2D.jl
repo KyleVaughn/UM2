73f472d165cc869e36c9c9df5bf3a623bcbd9f85
Kyle Vaughn <kcvaughn@umich.edu>
2021-11-20 16:05:30 -0500

    Minor updates for quadrilateral

--- src/Quadrilateral_2D.jl
+++ src/Quadrilateral_2D.jl
@@ -1,6 +1,9 @@
+# @code_warntype checked 2021/11/20
+
 # Quadrilateral in 2D defined by its 4 vertices.
-# NOFE: Quadrilaterals are assumed to be convex. This is because quadrilaterals must be convex
-# to be valid finite elements.
+# NOTE: Quadrilaterals are assumed to be convex. This is because quadrilaterals must be convex
+# to be valid finite elements, and we're generating meshes using a finite element mesh generator,
+# so this seems reasonable. See link below.
 # https://math.stackexchange.com/questions/2430691/jacobian-determinant-for-bi-linear-quadrilaterals
 struct Quadrilateral_2D{F <: AbstractFloat} <: Face_2D{F}
     # Counter clockwise order
@@ -9,6 +12,7 @@ end
 
 # Constructors
 # -------------------------------------------------------------------------------------------------
+# @code_warntype checked 2021/11/20
 Quadrilateral_2D(p₁::Point_2D,
                  p₂::Point_2D,
                  p₃::Point_2D,
@@ -16,6 +20,7 @@ Quadrilateral_2D(p₁::Point_2D,
 
 # Methods
 # -------------------------------------------------------------------------------------------------
+# @code_warntype checked 2021/11/20
 function (quad::Quadrilateral_2D{F})(r::R, s::S) where {F <: AbstractFloat,
                                                         R <: Real,
                                                         S <: Real}
@@ -29,37 +34,40 @@ function (quad::Quadrilateral_2D{F})(r::R, s::S) where {F <: AbstractFloat,
                  (1 - rₜ)*sₜ*quad.points[4]
 end
 
+# @code_warntype checked 2021/11/20
 function triangulate(quad::Quadrilateral_2D)
     # Return the two triangles that partition the domain
     A, B, C, D = quad.points
     return (Triangle_2D(A, B, C), Triangle_2D(C, D, A))
 end
 
+# @code_warntype checked 2021/11/20
 function area(quad::Quadrilateral_2D{F}) where {F <: AbstractFloat}
     # Using the convex quadrilateral assumption, just return the sum of the areas of the two
     # triangles that partition the quadrilateral. If the convex assumption ever changes, you
     # need to verify that the triangle pair partitions the quadrilateral. Choosing the wrong
     # pair overestimates the area, so just get the areas of both pairs of valid triangles and use
-    # the smaller area.
     return sum(area.(triangulate(quad)))
 end
 
+# @code_warntype checked 2021/11/20
 function in(p::Point_2D, quad::Quadrilateral_2D)
     return any(p .∈  triangulate(quad))
 end
 
+# @code_warntype checked 2021/11/20
 function intersect(l::LineSegment_2D{F}, quad::Quadrilateral_2D{F}) where {F <: AbstractFloat}
     # Create the 4 line segments that make up the quadrilateral and intersect each one
     line_segments = (LineSegment_2D(quad.points[1], quad.points[2]),
                      LineSegment_2D(quad.points[2], quad.points[3]),
                      LineSegment_2D(quad.points[3], quad.points[4]),
                      LineSegment_2D(quad.points[4], quad.points[1]))
-    intersections = l .∩ line_segments
     p₁ = Point_2D(F, 0)
     p₂ = Point_2D(F, 0)
     ipoints = 0x00
     # We need to account for 3 or 4 points returned due to vertex intersection
-    for (npoints, point) in intersections
+    for i = 1:4
+        npoints, point = l ∩ line_segments[i]
         if npoints === 0x01
             if ipoints === 0x00
                 p₁ = point
@@ -84,36 +92,35 @@ end
 
 # Plot
 # -------------------------------------------------------------------------------------------------
-function convert_arguments(P::Type{<:LineSegments}, quad::Quadrilateral_2D)
+function convert_arguments(LS::Type{<:LineSegments}, quad::Quadrilateral_2D)
     l₁ = LineSegment_2D(quad.points[1], quad.points[2])
     l₂ = LineSegment_2D(quad.points[2], quad.points[3])
     l₃ = LineSegment_2D(quad.points[3], quad.points[4])
     l₄ = LineSegment_2D(quad.points[4], quad.points[1])
     lines = [l₁, l₂, l₃, l₄]
-    return convert_arguments(P, lines)
+    return convert_arguments(LS, lines)
 end
 
-function convert_arguments(P::Type{<:LineSegments}, AQ::AbstractArray{<:Quadrilateral_2D})
-    point_sets = [convert_arguments(P, quad) for quad in AQ]
-    return convert_arguments(P, reduce(vcat, [pset[1] for pset in point_sets]))
+function convert_arguments(LS::Type{<:LineSegments}, Q::Vector{<:Quadrilateral_2D})
+    point_sets = [convert_arguments(LS, quad) for quad in Q]
+    return convert_arguments(LS, reduce(vcat, [pset[1] for pset in point_sets]))
 end
 
-function convert_arguments(P::Type{<:Mesh}, quad::Quadrilateral_2D)
+function convert_arguments(M::Type{<:Mesh}, quad::Quadrilateral_2D)
     points = [quad.points[i].x for i = 1:4]
     faces = [1 2 3;
              3 4 1]
-    return convert_arguments(P, points, faces)
+    return convert_arguments(M, points, faces)
 end
 
-function convert_arguments(MF::Type{Mesh{Tuple{Vector{Quadrilateral_2D{F}}}}},
-        AQ::Vector{Quadrilateral_2D{F}}) where {F <: AbstractFloat}
-    points = reduce(vcat, [[quad.points[i].x for i = 1:4] for quad in AQ])
-    faces = zeros(Int64, 2*length(AQ), 3)
+function convert_arguments(M::Type{<:Mesh}, Q::Vector{Quadrilateral_2D{F}}) where {F <: AbstractFloat}
+    points = reduce(vcat, [[quad.points[i].x for i = 1:4] for quad in Q])
+    faces = zeros(Int64, 2*length(Q), 3)
     j = 0
-    for i in 1:2:2*length(AQ)
+    for i in 1:2:2*length(Q)
         faces[i    , :] = [1 2 3] + [j j j]
         faces[i + 1, :] = [3 4 1] + [j j j]
         j += 4
     end
-    return convert_arguments(MF, points, faces)
+    return convert_arguments(M, points, faces)
 end
