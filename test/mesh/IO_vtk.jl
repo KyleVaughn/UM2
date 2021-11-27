6f74fcfe0854419329881425e9c684a5a83f6607
Kyle Vaughn <kcvaughn@umich.edu>
2021-11-27 11:34:50 -0500

    Fixed vtk tests

--- test/mesh/IO_vtk.jl
+++ test/mesh/IO_vtk.jl
@@ -8,14 +8,15 @@ using MOCNeutronTransport
         #   /     \   /     \
         #  /       \ /       \
         # 1---------2---------3
-        filepath = "./mesh_files/three_triangles.vtk"
+        filepath = "./mesh/mesh_files/three_triangles.vtk"
         ref_points = [Point_2D(0.0), Point_2D(2.0), Point_2D(4.0), Point_2D(1.0, 1.0), Point_2D(3.0, 1.0)]
         ref_faces = [
-                        (5, 1, 2, 4),
-                        (5, 2, 5, 4),
-                        (5, 2, 3, 5)
+                        SVector(5, 1, 2, 4),
+                        SVector(5, 2, 5, 4),
+                        SVector(5, 2, 3, 5)
                     ]
-        ref_edges = [(1, 2), (1, 4), (2, 3), (2, 4), (2, 5), (3, 5), (4, 5)]
+        ref_edges = [SVector(1, 2), SVector(1, 4), SVector(2, 3), 
+                     SVector(2, 4), SVector(2, 5), SVector(3, 5), SVector(4, 5)]
 
         # read_vtk
         mesh = read_vtk_2d(filepath)
@@ -25,7 +26,7 @@ using MOCNeutronTransport
 
         # write_vtk
         write_vtk_2d("write_three_triangle.vtk", mesh)
-        ref_file = open("./mesh_files/three_triangles.vtk", "r")
+        ref_file = open(filepath, "r")
         test_file = open("./write_three_triangle.vtk", "r")
         while !eof(ref_file)
             ref_line = readline(ref_file)
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,8 @@ using MOCNeutronTransport
 using Test
 using Logging
 Logging.disable_logging(Logging.Error)
-tests = ["./primitives/Point_2D",
+tests = ["Tree",
+         "./primitives/Point_2D",
          "./primitives/LineSegment_2D",
          "./primitives/QuadraticSegment_2D",
          "./primitives/Triangle_2D",
@@ -11,10 +12,8 @@ tests = ["./primitives/Point_2D",
          "./primitives/Quadrilateral8_2D",
          "./mesh/UnstructuredMesh_2D",
          "./mesh/IO_abaqus",
-#         "Tree",
+         "./mesh/IO_vtk",
 #         "AngularQuadrature", 
-#         "vtk",
-#         "xdmf"
         ]
 for t in tests
   include("$(t).jl")
