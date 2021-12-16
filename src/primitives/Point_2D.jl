523553c47a85e72dc7a230e9dc084c3e05885144
Kyle Vaughn <kcvaughn@umich.edu>
2021-12-16 15:41:33 -0500

    Point_2D Float64 update

--- src/primitives/Point_2D.jl
+++ src/primitives/Point_2D.jl
@@ -1,21 +1,13 @@
 # A 2D point in Cartesian coordinates.
-struct Point_2D{F <: AbstractFloat} <: FieldVector{2, F}
-    x::F
-    y::F
+struct Point_2D <: FieldVector{2, Float64}
+    x::Float64
+    y::Float64
 end
 
 # Constructors
 # -------------------------------------------------------------------------------------------------
 # 1D constructor
-Point_2D(x::F) where {F <: AbstractFloat} = Point_2D(x, F(0))
-
-# Single conversion constructor
-Point_2D(::Type{F}, x::X, y::Y) where {F <: AbstractFloat,
-                                       X <: Real,
-                                       Y <: Real} = Point_2D(F(x), F(y))
-# 1D single conversion constructor
-Point_2D(::Type{F}, x::X) where {F <: AbstractFloat,
-                                 X <: Real} = Point_2D(F(x), F(0))
+Point_2D(x::Real) = Point_2D(Float64(x), 0.0)
 
 # Operators (All type-stable)
 # -------------------------------------------------------------------------------------------------
@@ -26,13 +18,13 @@ Point_2D(::Type{F}, x::X) where {F <: AbstractFloat,
 # Hence the z coordinate of the resultant vector is returned.
 ×(p₁::Point_2D, p₂::Point_2D) = p₁.x*p₂.y - p₂.x*p₁.y
 ⋅(p₁::Point_2D, p₂::Point_2D) = p₁.x*p₂.x + p₁.y*p₂.y
-+(p::Point_2D, n::Real) = Point_2D(p.x + n, p.y + n)
++(p::Point_2D, n::Real) = p .+ n
 +(n::Real, p::Point_2D) = p + n
--(p::Point_2D, n::Real) = Point_2D(p.x - n, p.y - n)
+-(p::Point_2D, n::Real) = p .- n
 -(n::Real, p::Point_2D) = p - n
-*(n::Real, p::Point_2D) = Point_2D(n*p.x, n*p.y)
-*(p::Point_2D, n::Real) = n*p
-/(p::Point_2D, n::Real) = Point_2D(p.x/n, p.y/n)
+*(n::Real, p::Point_2D) = n .* p
+*(p::Point_2D, n::Real) = n * p
+/(p::Point_2D, n::Real) = p ./ n
 
 # Methods (All type-stable)
 # -------------------------------------------------------------------------------------------------
@@ -41,7 +33,7 @@ distance(p₁::Point_2D, p₂::Point_2D) = norm(p₁ - p₂)
 midpoint(p₁::Point_2D, p₂::Point_2D) = (p₁ + p₂)/2
 
 # Sort points based on their distance from a given point
-function sort_points(p::Point_2D{F}, points::Vector{Point_2D{F}}) where {F <: AbstractFloat}
+function sort_points(p::Point_2D, points::Vector{Point_2D})
     if 0 < length(points)
         # Sort the points based upon their distance to the point
         return points[sortperm(distance.(Ref(p), points))] 
