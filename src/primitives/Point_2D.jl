67854c288696720b7ce828bf0f82dbdcc538605e
Kyle Vaughn <kcvaughn@umich.edu>
2022-01-06 20:27:32 -0500

    Working on minor changes to primitives

--- src/primitives/Point_2D.jl
+++ src/primitives/Point_2D.jl
@@ -15,24 +15,24 @@ Base.broadcastable(p::Point_2D) = Ref(p)
 
 # Operators
 # -------------------------------------------------------------------------------------------------
-≈(p₁::Point_2D, p₂::Point_2D) = distance²(p₁, p₂) < Point_2D_differentiation_distance^2
+@inline ≈(p₁::Point_2D, p₂::Point_2D) = distance²(p₁, p₂) < Point_2D_differentiation_distance^2
 # Note the cross product of two 2D points returns a scalar. It is assumed that this is the
 # desired quantity, since the cross product of vectors in the plane is a vector normal to the plane.
 # Hence the z coordinate of the resultant vector is returned.
-×(p₁::Point_2D, p₂::Point_2D) = p₁.x*p₂.y - p₂.x*p₁.y
-⋅(p₁::Point_2D, p₂::Point_2D) = p₁.x*p₂.x + p₁.y*p₂.y
-+(p::Point_2D, n::Real) = Point_2D(p.x + n, p.y + n)
-+(n::Real, p::Point_2D) = p + n
--(p::Point_2D, n::Real) = Point_2D(p.x - n, p.y - n)
--(n::Real, p::Point_2D) = p - n
+@inline ×(p₁::Point_2D, p₂::Point_2D) = p₁.x*p₂.y - p₂.x*p₁.y
+@inline ⋅(p₁::Point_2D, p₂::Point_2D) = p₁.x*p₂.x + p₁.y*p₂.y
+@inline +(p::Point_2D, n::Real) = Point_2D(p.x + n, p.y + n)
+@inline +(n::Real, p::Point_2D) = Point_2D(p.x + n, p.y + n)
+@inline -(p::Point_2D, n::Real) = Point_2D(p.x - n, p.y - n)
+@inline -(n::Real, p::Point_2D) = Point_2D(n - p.x, n - p.y)
 
 # Methods
 # -------------------------------------------------------------------------------------------------
-norm(p::Point_2D) = sqrt(p.x^2 + p.y^2)
-norm²(p::Point_2D) = p.x^2 + p.y^2
-distance(p₁::Point_2D, p₂::Point_2D) = norm(p₁ - p₂)
-distance²(p₁::Point_2D, p₂::Point_2D) = (p₁ - p₂) ⋅(p₁ - p₂)
-midpoint(p₁::Point_2D, p₂::Point_2D) = (p₁ + p₂)/2
+@inline norm(p::Point_2D) = sqrt(p.x^2 + p.y^2)
+@inline norm²(p::Point_2D) = p.x^2 + p.y^2
+@inline distance(p₁::Point_2D, p₂::Point_2D) = norm(p₁ - p₂)
+@inline distance²(p₁::Point_2D, p₂::Point_2D) = (p₁ - p₂) ⋅(p₁ - p₂)
+@inline midpoint(p₁::Point_2D, p₂::Point_2D) = (p₁ + p₂)/2
 
 # Sort points based on their distance from a given point
 function sortpoints(p::Point_2D, points::Vector{Point_2D})
