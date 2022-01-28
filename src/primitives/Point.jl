fc41103ebab8a8f87eead953feca498213588e8f
Kyle Vaughn <kcvaughn@umich.edu>
2022-01-28 05:15:47 -0500

    Point update

--- src/primitives/Point.jl
+++ src/primitives/Point.jl
@@ -1,10 +1,9 @@
 # An Dim-dimensional point
-struct Point{Dim,T}
-    coord::SVector{Dim,T}
+struct Point{Dim, T}
+    coord::SVector{Dim, T}
 end
 
 const Point2D = Point{2}
-const Point3D = Point{3}
 
 Base.broadcastable(p::Point) = Ref(p)
 Base.@propagate_inbounds function Base.getindex(p::Point, i::Integer)
@@ -49,76 +48,14 @@ Point(x...) = Point(SVector(x))
 @inline distance(p₁::Point, v::Vector) = norm(p - v)
 @inline distance(v::Vector, p::Point) = norm(v - p)
 @inline distance²(p₁::Point, p₂::Point) = norm²(p₁ - p₂)
-@inline midpoint(p₁::Point{Dim,T}, p₂::Point{Dim,T}) where {Dim,T} = Point{Dim,T}((p₁ + p₂)/2)
+@inline midpoint(p₁::Point, p₂::Point) = (p₁ + p₂)/2
 @inline norm(p::Point) = √(p.coord ⋅ p.coord)
 @inline norm²(p::Point) = p.coord ⋅ p.coord
-
 Base.zero(::Type{Point{Dim,T}}) where {Dim,T} = Point{Dim,T}(@SVector zeros(T, Dim))
-
-# Random points
+# Random point in the Dim-dimensional unit hypercube
 Base.rand(::Type{Point{Dim,T}}) where {Dim,T} = Point{Dim,T}(rand(SVector{Dim,T}))
-function Base.rand(::Type{Point{Dim,T}}, NP::Int64) where {Dim,T}
-    return [ Point{Dim,T}(rand(SVector{Dim,T})) for i = 1:NP ]
-end
-
-# Sort points based on their distance from a given point using insertion sort
-# The intended use is points from ray tracing, which should be nearly sorted or sorted, 
-# so insertion sort is quick
-function sortpoints!(p::Point, points::Vector{<:Point})
-    npts = length(points)
-    for i ∈ 2:npts
-        j = i - 1
-        dist = distance²(p, points[i])
-        pt = points[i]
-        while 0 < j 
-            if dist < distance²(p, points[j]) 
-                points[j+1] = points[j]
-                j -= 1
-                continue
-            end
-            break
-        end
-        points[j+1] = pt
-    end
-    return nothing
-end
-function sortpoints(p::Point, points::Vector{<:Point})
-    points_sorted = deepcopy(points)
-    npts = length(points)
-    for i ∈ 2:npts
-        j = i - 1
-        dist = distance²(p, points_sorted[i])
-        pt = points[i]
-        while 0 < j 
-            if dist < distance²(p, points_sorted[j]) 
-                points_sorted[j+1] = points_sorted[j]
-                j -= 1
-                continue
-            end
-            break
-        end
-        points_sorted[j+1] = pt
-    end
-    return points_sorted
-end
-
-# Sort intersection points, deleting points that are less than minimum_segment_length apart
-function sort_intersection_points!(p::Point, points::Vector{<:Point})
-    sortpoints!(p, points)
-    # Eliminate any points for which the distance between consecutive points
-    # is less than the minimum segment length
-    id_start = 1
-    id_stop = 2
-    npoints = length(points)
-    while id_stop <= npoints
-        if distance²(points[id_start], points[id_stop]) < minimum_segment_length^2
-            deleteat!(points, id_stop)
-            npoints -= 1
-        else
-            id_start = id_stop
-            id_stop += 1
-        end
-    end
+function Base.rand(::Type{Point{Dim,T}}, num_points::Int64) where {Dim,T}
+    return [ Point{Dim,T}(rand(SVector{Dim,T})) for i = 1:num_points ]
 end
 
 # Plot
