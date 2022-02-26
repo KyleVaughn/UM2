7e4d53241d85617398277b8067436e330f266c9f
kcvaughn <kcvaughn@umich.edu>
2022-02-26 00:34:48 -0500

    QuadraticHexahedron interpolation fixed

--- test/interpolation.jl
+++ test/interpolation.jl
@@ -127,9 +127,77 @@
         end
     end
 
-
-
-
-
-
+    @testset "QuadraticTetrahedron" begin
+        for T in [Float32, Float64, BigFloat]
+            p₁  = Point3D{T}(0, 0, 0)
+            p₂  = Point3D{T}(1, 0, 0)
+            p₃  = Point3D{T}(0, 1, 0)
+            p₄  = Point3D{T}(0, 0, 1)
+            p₅  = Point3D{T}(1//2,    0,    0)
+            p₆  = Point3D{T}(1//2, 1//2,    0)
+            p₇  = Point3D{T}(0,    1//2,    0)
+            p₈  = Point3D{T}(0,       0, 1//2)
+            p₉  = Point3D{T}(1//2,    0, 1//2)
+            p₁₀ = Point3D{T}(0,    1//2, 1//2)
+            tet10 = QuadraticTetrahedron(p₁, p₂, p₃, p₄, p₅, p₆, p₇, p₈, p₉, p₁₀)
+            @test tet10(0, 0, 0) ≈ p₁ 
+            @test tet10(1, 0, 0) ≈ p₂ 
+            @test tet10(0, 1, 0) ≈ p₃ 
+            @test tet10(0, 0, 1) ≈ p₄ 
+            @test tet10(1//2,    0,    0) ≈ p₅
+            @test tet10(1//2, 1//2,    0) ≈ p₆ 
+            @test tet10(0,    1//2,    0) ≈ p₇ 
+            @test tet10(0,       0, 1//2) ≈ p₈ 
+            @test tet10(1//2,    0, 1//2) ≈ p₉ 
+            @test tet10(0,    1//2, 1//2) ≈ p₁₀ 
+        end
+    end
+    
+    @testset "QuadraticHexahedon" begin
+        for T in [Float32, Float64, BigFloat]
+            p₁  = Point3D{T}(0, 0, 0)
+            p₂  = Point3D{T}(1, 0, 0)
+            p₃  = Point3D{T}(1, 1, 0)
+            p₄  = Point3D{T}(0, 1, 0)
+            p₅  = Point3D{T}(0, 0, 1)
+            p₆  = Point3D{T}(1, 0, 1)
+            p₇  = Point3D{T}(1, 1, 1)
+            p₈  = Point3D{T}(0, 1, 1)
+            p₉  = Point3D{T}(1//2,    0,    0)
+            p₁₀ = Point3D{T}(   1, 1//2,    0)
+            p₁₁ = Point3D{T}(1//2,    1,    0)
+            p₁₂ = Point3D{T}(   0, 1//2,    0)
+            p₁₃ = Point3D{T}(1//2,    0,    1)
+            p₁₄ = Point3D{T}(   1, 1//2,    1)
+            p₁₅ = Point3D{T}(1//2,    1,    1)
+            p₁₆ = Point3D{T}(   0, 1//2,    1)
+            p₁₇ = Point3D{T}(0, 0, 1//2)
+            p₁₈ = Point3D{T}(1, 0, 1//2)
+            p₁₉ = Point3D{T}(1, 1, 1//2)
+            p₂₀ = Point3D{T}(0, 0, 1//2)
+            hex20 = QuadraticHexahedron(p₁,  p₂,  p₃,  p₄,  p₅,  p₆,  p₇,
+                                        p₈,  p₉,  p₁₀, p₁₁, p₁₂, p₁₃, p₁₄, 
+                                        p₁₅, p₁₆, p₁₇, p₁₈, p₁₉, p₂₀)
+            @test hex20(0, 0, 0) ≈ p₁
+            @test hex20(1, 0, 0) ≈ p₂
+            @test hex20(1, 1, 0) ≈ p₃
+            @test hex20(0, 1, 0) ≈ p₄
+            @test hex20(0, 0, 1) ≈ p₅
+            @test hex20(1, 0, 1) ≈ p₆
+            @test hex20(1, 1, 1) ≈ p₇
+            @test hex20(0, 1, 1) ≈ p₈
+            @test hex20(1//2,    0,    0)≈ p₉
+            @test hex20(   1, 1//2,    0)≈ p₁₀    
+            @test hex20(1//2,    1,    0)≈ p₁₁
+            @test hex20(   0, 1//2,    0)≈ p₁₂
+            @test hex20(1//2,    0,    1)≈ p₁₃
+            @test hex20(   1, 1//2,    1)≈ p₁₄
+            @test hex20(1//2,    1,    1)≈ p₁₅
+            @test hex20(   0, 1//2,    1)≈ p₁₆
+            @test hex20(0, 0, 1//2)≈ p₁₇
+            @test hex20(1, 0, 1//2)≈ p₁₈
+            @test hex20(1, 1, 1//2)≈ p₁₉
+            @test hex20(0, 0, 1//2)≈ p₂₀
+        end
+    end
 end
