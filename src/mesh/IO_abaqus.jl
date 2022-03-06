8675cdd3beca8e9d9bfaa54835c32c9a837955f6
kcvaughn <kcvaughn@umich.edu>
2022-03-06 00:01:27 -0500

    Working on abaqus IO

--- src/mesh/IO_abaqus.jl
+++ src/mesh/IO_abaqus.jl
@@ -1,26 +1,33 @@
 # IO routines for the Abaqus .inp file format
-const valid_abaqus_types = (
-    # 2D
+const valid_abaqus_element_types = (
     # triangle
-    "CPS3", "SFRU3",
-    # triangle6
+    "CPS3",
+    # quadratic triangle
     "CPS6",
     # quadrilateral
     "CPS4", 
-    # quad8
+    # quadratic quadrilateral
     "CPS8",
-    "M3D9"
+    # tetrahedron
+    "C3D4",
+    # hexahedron
+    "C3D8",
+    # quadratic tetrahedron
+    "C3D10",
+    # quadratic hexahedron"
+    "C3D20"
    )
 
-function read_abaqus2d(filepath::String, floattype::Type{T}=Float64) where {T<:AbstractFloat}
+function read_abaqus(filepath::String, floattype::Type{T}=Float64) where {T<:AbstractFloat}
     @info "Reading "*filepath
     # NOTE: There is a critical assumption here that elements and nodes are listed 1 to N,
     # not 8, 10, 9 or anything funky/out of order.
     file = open(filepath, "r")
     name = "default_name"
-    faces_vecs = Vector{UInt64}[] 
-    face_sets = Dict{String, BitSet}()
-    points = Point2D{floattype}[]
+    element_vecs = Vector{UInt64}[] 
+    element_sets = Dict{String, BitSet}()
+    points = Point3D{floattype}[]
+    is3D = false
     while !eof(file)
         line = readline(file)
         if length(line) > 0
@@ -32,23 +39,26 @@ function read_abaqus2d(filepath::String, floattype::Type{T}=Float64) where {T<:A
                     name = name[1:length(name)-4]
                 end
             elseif "*NODE" == line
-                _read_abaqus_nodes_2d!(file, points)
+                _read_abaqus_nodes!(file, points)
             elseif occursin("*ELEMENT", line)
                 linesplit = split(line)
                 element_type = String(strip(replace(linesplit[2], ("type=" => "")), ','))
-                _read_abaqus_elements!(file, faces_vecs, element_type)
+                if element_type[1:2] == "C3"
+                    is2D = true
+                end
+                _read_abaqus_elements!(file, elements_vecs, element_type)
             elseif occursin("*ELSET", line)
                 linesplit = split(line)
                 set_name = String(replace(linesplit[1], ("*ELSET,ELSET=" => "")))
-                face_sets[set_name] = _read_abaqus_elset(file)
+                element_sets[set_name] = _read_abaqus_elset(file)
             end
         end
     end
     close(file)
-    return _create_2d_mesh_from_vector_faces(name, points, faces_vecs, face_sets)
+    return _create_mesh_from_elements(is3D, name, points, elements_vecs, element_sets)
 end
 
-function _read_abaqus_nodes_2d!(file::IOStream, points::Vector{Point2D{T}}) where {T}
+function _read_abaqus_nodes!(file::IOStream, points::Vector{Point3D{T}}) where {T}
     # Count the number of nodes
     file_position = position(file)
     npoints = 0
@@ -59,13 +69,13 @@ function _read_abaqus_nodes_2d!(file::IOStream, points::Vector{Point2D{T}}) wher
     end
     seek(file, file_position)
     # Allocate and populate a vector of points
-    new_points = Vector{Point2D{T}}(undef, npoints)
+    new_points = Vector{Point3D{T}}(undef, npoints)
     line = readline(file)
     ipt = 0
     while !('*' == line[1])
         ipt += 1
-        xy = parse.(T, strip.(view(split(line),2:3), [',']))
-        new_points[ipt] = Point2D(xy[1], xy[2])
+        xyz = parse.(T, strip.(view(split(line),2:4), [',']))
+        new_points[ipt] = Point(xyz[1], xyz[2], xyz[3])
         file_position = position(file)
         line = readline(file)
     end
@@ -74,7 +84,7 @@ function _read_abaqus_nodes_2d!(file::IOStream, points::Vector{Point2D{T}}) wher
     return nothing
 end
 
-function _read_abaqus_elements!(file::IOStream, faces::Vector{Vector{UInt64}}, 
+function _read_abaqus_elements!(file::IOStream, elements::Vector{Vector{UInt64}}, 
                                 element_type::String)
     if !(element_type ∈ valid_abaqus_types)  
         error("$element_type is not in the valid abaqus types")
@@ -83,12 +93,8 @@ function _read_abaqus_elements!(file::IOStream, faces::Vector{Vector{UInt64}},
     file_position = position(file)
     while !('*' == line[1] || eof(file))
         linesplit = split(line)
-        vertexIDs = parse.(UInt64, strip.(view(linesplit, 2:length(linesplit)), [',']))
-        if length(vertexIDs) == 9
-            push!(faces, vertexIDs[1:8])
-        else
-            push!(faces, vertexIDs)
-        end
+        vertex_ids = parse.(UInt64, strip.(view(linesplit, 2:length(linesplit)), [',']))
+        push!(elements, vertex_ids)
         file_position = position(file)
         line = readline(file)
     end
