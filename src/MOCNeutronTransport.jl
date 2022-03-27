32ecffeba09ec3260f72bf3cbf55919f7f7b4e4a
Kyle Vaughn <kcvaughn@umich.edu>
2022-03-27 19:33:43 -0400

    Better gmsh included test

--- src/MOCNeutronTransport.jl
+++ src/MOCNeutronTransport.jl
@@ -17,20 +17,20 @@ import LinearAlgebra: ×, ⋅, norm, inv
 
 # include gmsh
 # Check if there is a local install on JULIA_LOAD_PATH
-gmsh_found = false
 for path in Base.load_path()
     gmsh_jl = joinpath(path, "gmsh.jl")
     if isfile(gmsh_jl)
-        @info "Using the gmsh API found at: "*gmsh_jl
+        @info "MOCNeutronTransport is using the gmsh API found at: "*gmsh_jl
         include(gmsh_jl)
         break
     end
 end
 # Fallback on the SDK binary
-if !gmsh_found
+if !@isdefined(gmsh)
     gmsh_dir = readdir(artifact"gmsh", join=true)[1]
     gmsh_jl = joinpath(gmsh_dir, "lib", "gmsh.jl") 
     if isfile(gmsh_jl)
+        @info "MOCNeutronTransport is using the gmsh API found at: "*gmsh_jl
         include(gmsh_jl)
     else
         error("Could not find gmsh API.")
