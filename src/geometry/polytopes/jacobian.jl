809ccbbe6297d136f0c7c9698ee873a3a51f870b
Kyle Vaughn <kcvaughn@umich.edu>
2022-05-02 20:43:51 -0400

    Jacobian update

--- src/geometry/polytopes/jacobian.jl
+++ src/geometry/polytopes/jacobian.jl
@@ -6,26 +6,26 @@ jacobian(q::QuadraticSegment, r) = (4r - 3)*(q[1] - q[3]) + (4r - 1)*(q[2] - q[3
 
 # 2-polytopes
 function jacobian(tri::Triangle, r, s)
-    ∂F_∂r = tri[2] - tri[1]
-    ∂F_∂s = tri[3] - tri[1]
-    return hcat(∂F_∂r, ∂F_∂s)
+    ∂ᵣF = tri[2] - tri[1]
+    ∂ₛF = tri[3] - tri[1]
+    return hcat(∂ᵣF, ∂ₛF)
 end
 
 function jacobian(quad::Quadrilateral, r, s)
-    ∂F_∂r = (1 - s)*(quad[2] - quad[1]) + s*(quad[3] - quad[4])
-    ∂F_∂s = (1 - r)*(quad[4] - quad[1]) + r*(quad[3] - quad[2])
-    return hcat(∂F_∂r, ∂F_∂s)
+    ∂ᵣF = (1 - s)*(quad[2] - quad[1]) + s*(quad[3] - quad[4])
+    ∂ₛF = (1 - r)*(quad[4] - quad[1]) + r*(quad[3] - quad[2])
+    return hcat(∂ᵣF, ∂ₛF)
 end
 
 function jacobian(tri6::QuadraticTriangle, r, s)
-    ∂F_∂r = (4r + 4s - 3)*(tri6[1] - tri6[4]) +
-                 (4r - 1)*(tri6[2] - tri6[4]) +
-                     (4s)*(tri6[5] - tri6[6])
+    ∂ᵣF = (4r + 4s - 3)*(tri6[1] - tri6[4]) +
+               (4r - 1)*(tri6[2] - tri6[4]) +
+                   (4s)*(tri6[5] - tri6[6])
 
-    ∂F_∂s = (4r + 4s - 3)*(tri6[1] - tri6[6]) +
-                 (4s - 1)*(tri6[3] - tri6[6]) +
-                     (4r)*(tri6[5] - tri6[4])
-    return hcat(∂F_∂r, ∂F_∂s)
+    ∂ₛF = (4r + 4s - 3)*(tri6[1] - tri6[6]) +
+               (4s - 1)*(tri6[3] - tri6[6]) +
+                   (4r)*(tri6[5] - tri6[4])
+    return hcat(∂ᵣF, ∂ₛF)
 end
 
 function jacobian(quad8::QuadraticQuadrilateral, r, s)
@@ -64,48 +64,48 @@ end
 
 # 3-polytopes
 function jacobian(tet::Tetrahedron, r, s, t)
-    ∂F_∂r = tet[2] - tet[1]
-    ∂F_∂s = tet[3] - tet[1]
-    ∂F_∂t = tet[4] - tet[1]
-    return hcat(∂F_∂r, ∂F_∂s, ∂F_∂t)
+    ∂ᵣF = tet[2] - tet[1]
+    ∂ₛF = tet[3] - tet[1]
+    ∂ₜF = tet[4] - tet[1]
+    return hcat(∂ᵣF, ∂ₛF, ∂ₜF)
 end
 
 function jacobian(hex::Hexahedron, r, s, t)
-    ∂F_∂r = ((1 - s)*(1 - t))*(hex[2] - hex[1]) + 
-            ((    s)*(1 - t))*(hex[3] - hex[4]) +
-            ((1 - s)*(    t))*(hex[6] - hex[5]) +
-            ((    s)*(    t))*(hex[7] - hex[8])
+    ∂ᵣF = ((1 - s)*(1 - t))*(hex[2] - hex[1]) + 
+          ((    s)*(1 - t))*(hex[3] - hex[4]) +
+          ((1 - s)*(    t))*(hex[6] - hex[5]) +
+          ((    s)*(    t))*(hex[7] - hex[8])
 
-    ∂F_∂s = ((1 - r)*(1 - t))*(hex[4] - hex[1]) + 
-            ((    r)*(1 - t))*(hex[3] - hex[2]) +
-            ((1 - r)*(    t))*(hex[8] - hex[5]) +
-            ((    r)*(    t))*(hex[7] - hex[6])
+    ∂ₛF = ((1 - r)*(1 - t))*(hex[4] - hex[1]) + 
+          ((    r)*(1 - t))*(hex[3] - hex[2]) +
+          ((1 - r)*(    t))*(hex[8] - hex[5]) +
+          ((    r)*(    t))*(hex[7] - hex[6])
 
-    ∂F_∂t = ((1 - r)*(1 - s))*(hex[5] - hex[1]) + 
-            ((    r)*(1 - s))*(hex[6] - hex[2]) +
-            ((1 - r)*(    s))*(hex[8] - hex[4]) +
-            ((    r)*(    s))*(hex[7] - hex[3])
-    return hcat(∂F_∂r, ∂F_∂s, ∂F_∂t)
+    ∂ₜF = ((1 - r)*(1 - s))*(hex[5] - hex[1]) + 
+          ((    r)*(1 - s))*(hex[6] - hex[2]) +
+          ((1 - r)*(    s))*(hex[8] - hex[4]) +
+          ((    r)*(    s))*(hex[7] - hex[3])
+    return hcat(∂ᵣF, ∂ₛF, ∂ₜF)
 end
 
 function jacobian(tet::QuadraticTetrahedron, r, s, t)
     u = 1 - r - s - t
-    ∂F_∂r =      (tet[ 1] - tet[2]) +
-            (4u)*(tet[ 5] - tet[1]) +
-            (4r)*(tet[ 2] - tet[5]) +
-            (4s)*(tet[ 6] - tet[7]) +
-            (4t)*(tet[ 9] - tet[8]) 
+    ∂ᵣF =      (tet[ 1] - tet[2]) +
+          (4u)*(tet[ 5] - tet[1]) +
+          (4r)*(tet[ 2] - tet[5]) +
+          (4s)*(tet[ 6] - tet[7]) +
+          (4t)*(tet[ 9] - tet[8]) 
 
-    ∂F_∂s =      (tet[ 1] - tet[3]) +
-            (4u)*(tet[ 7] - tet[1]) +
-            (4r)*(tet[ 6] - tet[5]) +
-            (4s)*(tet[ 3] - tet[7]) +
-            (4t)*(tet[10] - tet[8]) 
+    ∂ₛF =      (tet[ 1] - tet[3]) +
+          (4u)*(tet[ 7] - tet[1]) +
+          (4r)*(tet[ 6] - tet[5]) +
+          (4s)*(tet[ 3] - tet[7]) +
+          (4t)*(tet[10] - tet[8]) 
 
-    ∂F_∂t =      (tet[ 1] - tet[4]) +
-            (4u)*(tet[ 8] - tet[1]) +
-            (4r)*(tet[ 9] - tet[5]) +
-            (4s)*(tet[10] - tet[7]) +
-            (4t)*(tet[ 4] - tet[8]) 
-    return hcat(∂F_∂r, ∂F_∂s, ∂F_∂t)
+    ∂ₜF =      (tet[ 1] - tet[4]) +
+          (4u)*(tet[ 8] - tet[1]) +
+          (4r)*(tet[ 9] - tet[5]) +
+          (4s)*(tet[10] - tet[7]) +
+          (4t)*(tet[ 4] - tet[8]) 
+    return hcat(∂ᵣF, ∂ₛF, ∂ₜF)
 end
