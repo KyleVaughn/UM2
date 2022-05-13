72b59c1e0b5fb0f69979214b36f84b00df3745e9
Kyle Vaughn <kcvaughn@umich.edu>
2022-05-13 12:22:39 -0400

    More efficient rect grid intersection

--- src/raytracing/intersect/linesegment-rectilinear_grid.jl
+++ src/raytracing/intersect/linesegment-rectilinear_grid.jl
@@ -1,9 +1,9 @@
 function Base.intersect(l::LineSegment{Point{2,T}}, 
                         g::RectilinearGrid{X,Y,0,T}) where {X,Y,T} 
-    𝘂⁻¹ = 1 ./ (l[2] - l[1])
-    X₁ = coordinates(l[1])
     # Intersect the bounding box
     # https://tavianator.com/2011/ray_box.html
+    𝘂⁻¹ = 1 ./ (l[2] - l[1])
+    X₁ = coordinates(l[1])
     r₁ = 𝘂⁻¹ ⊙ (Vec(xmin(g), ymin(g)) - X₁)
     r₂ = 𝘂⁻¹ ⊙ (Vec(xmax(g), ymax(g)) - X₁)
     rmin = maximum(min.(r₁, r₂))
@@ -16,13 +16,12 @@ function Base.intersect(l::LineSegment{Point{2,T}},
         xlower, xupper = minmax(pstart[1], pend[1])
         ylower, yupper = minmax(pstart[2], pend[2])
         # Get the start and stop indices for the range of grid lines
+        # that need to be tested
         xlower_ind = searchsortedfirst(g.x, xlower)
         xupper_ind = searchsortedfirst(g.x, xupper)
         ylower_ind = searchsortedfirst(g.y, ylower)
         yupper_ind = searchsortedfirst(g.y, yupper)
-        # Only intersect the lines that are within the AABB formed by
-        # pstart, pend. If the line is oriented in the negative direction, 
-        # we need to decrement instead of increment in index
+
         xstart, xend = xlower_ind, xupper_ind
         xinc = 1
         if pstart[1] > pend[1]
@@ -39,57 +38,67 @@ function Base.intersect(l::LineSegment{Point{2,T}},
         end
         iygen = ystart:yinc:yend
 
-        intersections = Point{2,T}[] 
+        # Allocate for the max number of intersections and delete the end 
+        # if not needed
         nx = length(ixgen)
         ny = length(iygen)
+        nmax = nx + ny
+        intersections = Vector{Point{2,T}}(undef, nmax) 
         r = 𝘂⁻¹[1]*(g.x[xstart] - X₁[1])
         s = 𝘂⁻¹[2]*(g.y[ystart] - X₁[2])
         i = 2
         j = 2
+        k = 1
+        # Add the intersection with smallest valid parametric coordinate
+        # until we run out of x or y values to test
         while i ≤ nx && j ≤ ny
             if r < s
                 if rmin ≤ r ≤ rmax
-                    push!(intersections, l(r))
+                    intersections[k] = l(r)
+                    k += 1
                 end
                 r = 𝘂⁻¹[1]*(g.x[ixgen[i]] - X₁[1])
                 i += 1
             else
                 if rmin ≤ s ≤ rmax
-                    push!(intersections, l(s))
+                    intersections[k] = l(s)
+                    k += 1
                 end
                 s = 𝘂⁻¹[2]*(g.y[iygen[j]] - X₁[2])
                 j += 1
             end
         end
 
+        # Set the remaining intersections
         while i ≤ nx
             if rmin ≤ r ≤ rmax
-                push!(intersections, l(r))
+                intersections[k] = l(r)
+                k += 1
             end
             r = 𝘂⁻¹[1]*(g.x[ixgen[i]] - X₁[1])
             i += 1
         end
         if rmin ≤ r ≤ rmax
-            push!(intersections, l(r))
+            intersections[k] = l(r)
+            k += 1
         end
 
         while j ≤ ny
             if rmin ≤ s ≤ rmax
-                push!(intersections, l(s))
+                intersections[k] = l(s)
+                k += 1
             end
             s = 𝘂⁻¹[2]*(g.y[iygen[j]] - X₁[2])
             j += 1
         end
         if rmin ≤ s ≤ rmax
-            push!(intersections, l(s))
+            intersections[k] = l(s)
+            k += 1
         end
-
+        # Delete the unused end of the vector
+        Base._deleteend!(intersections, nmax - k + 1)
         return intersections
     else
         return Point{2,T}[]
     end
 end
-
-# Test line that clips a corner and no other lines
-# Tsst line that is vertical or horizontal
-# 45 degree angle
