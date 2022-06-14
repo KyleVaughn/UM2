97cae6d50d2d09a5bcd3399a36a8b3ededcd996a
Kyle Vaughn <kcvaughn@umich.edu>
2022-06-14 15:50:53 -0400

    edges, faces, & materialize for volume mesh

--- src/mesh/edges.jl
+++ src/mesh/edges.jl
@@ -1,17 +1,19 @@
+export edge_connectivity
+
 function edge_connectivity(mesh::VolumeMesh{2, T, U}) where {T, U}
     if islinear(mesh)
-        return _volume_mesh_linear_edges(mesh)
+        return _volume_mesh_linear_edge_connectivity(mesh)
     else
-        return _volume_mesh_quadratic_edges(mesh)
+        return _volume_mesh_quadratic_edge_connectivity(mesh)
     end
 end
 
-function _volume_mesh_linear_edges(mesh::VolumeMesh{2, T, U}) where {T, U}
-    unique_edges = Vec{2,U}[]
+function _volume_mesh_linear_edge_connectivity(mesh::VolumeMesh{2, T, U}) where {T, U}
+    unique_edges = Vec{2, U}[]
     nedges = 0
-    for face ∈ mesh.offsets
+    for face in face_connectivity(mesh)
         edge_vecs = edges(face)
-        for edge ∈ edge_vecs
+        for edge::LineSegment{U} in edge_vecs
             if edge[1] < edge[2]
                 sorted_edge = edge.vertices
             else
@@ -24,9 +26,30 @@ function _volume_mesh_linear_edges(mesh::VolumeMesh{2, T, U}) where {T, U}
             end
         end
     end
-    materialized_edges = Vector{LineSegment{Point{D,T}}}(undef, nedges)
-    for i = 1:nedges
-        materialized_edges[i] = materialize(LineSegment(unique_edges[i]), mesh.vertices)
+    lines = Vector{LineSegment{U}}(undef, nedges)
+    lines[1:nedges] = reinterpret(LineSegment{U}, unique_edges)
+    return lines
+end 
+
+function _volume_mesh_quadratic_edge_connectivity(mesh::VolumeMesh{2, T, U}) where {T, U}
+    unique_edges = Vec{3, U}[]
+    nedges = 0
+    for face in face_connectivity(mesh)
+        edge_vecs = edges(face)
+        for edge::QuadraticSegment{U} in edge_vecs
+            if edge[1] < edge[2]
+                sorted_edge = edge.vertices
+            else
+                sorted_edge = Vec(edge[2], edge[1], edge[3])
+            end
+            index = getsortedfirst(unique_edges, sorted_edge)
+            if nedges < index || unique_edges[index] !== sorted_edge
+                insert!(unique_edges, index, sorted_edge)
+                nedges += 1
+            end
+        end
     end
-    return materialized_edges 
+    segs = Vector{QuadraticSegment{U}}(undef, nedges)
+    segs[1:nedges] = reinterpret(QuadraticSegment{U}, unique_edges)
+    return segs
 end 
--- src/mesh/materialize.jl
+++ src/mesh/materialize.jl
@@ -1,5 +1,5 @@
 export materialize,
-       materialize_edge,
+#       materialize_edge,
        materialize_face,
        materialize_cell,
        materialize_polytope,
@@ -14,67 +14,18 @@ function materialize(p::Polytope{K, P, N, T},
                                                      Val(N))))
 end
 
-# materialize_edge
-# materialize_face
+function materialize_edges(mesh::VolumeMesh{2})
+    return materialize.(edge_connectivity(mesh), Ref(mesh.points))
+end
 
+function materialize_face(i::Integer, mesh::VolumeMesh{2})
+    return materialize(_materialize_face_connectivity(i, mesh), mesh.points)
+end
 
+function materialize_faces(mesh::VolumeMesh{2})
+    return materialize.(face_connectivity(mesh), Ref(mesh.points))
+end
 
-## Not type-stable
-#function materialize_face(i::Integer, mesh::VolumeMesh{2})
-#    offset = mesh.offsets[i]
-#    Δ = offset_diff(i, mesh)
-#    points = mesh.points
-#    conn = mesh.connectivity
-#    if Δ == 3
-#        vids = Vec{3, Int64}(conn[Vec(ntuple(i -> i + offset - 1, Val(3))...)])
-#        return Triangle(points[vids])
-#    elseif Δ == 4
-#        vids = Vec{4, Int64}(conn[Vec(ntuple(i -> i + offset - 1, Val(4))...)])
-#        return Quadrilateral(points[vids])
-#    elseif Δ == 6
-#        vids = Vec{6, Int64}(conn[Vec(ntuple(i -> i + offset - 1, Val(6))...)])
-#        return QuadraticTriangle(points[vids])
-#    elseif Δ == 8
-#        vids = Vec{8, Int64}(conn[Vec(ntuple(i -> i + offset - 1, Val(8))...)])
-#        return QuadraticQuadrilateral(points[vids])
-#    else
-#        error("Unsupported type.")
-#        return nothing
-#    end
-#end
-#
-## Not type-stable
-#function _materialize_face_connectivity(i::Integer, mesh::VolumeMesh{2})
-#    offset = mesh.offsets[i]
-#    Δ = offset_diff(i, mesh)
-#    conn = mesh.connectivity
-#    if Δ == 3
-#        vids = conn[Vec(ntuple(i -> i + offset - 1, Val(3))...)]
-#        return Triangle(vids)
-#    elseif Δ == 4
-#        vids = conn[Vec(ntuple(i -> i + offset - 1, Val(4))...)]
-#        return Quadrilateral(vids)
-#    elseif Δ == 6
-#        vids = conn[Vec(ntuple(i -> i + offset - 1, Val(6))...)]
-#        return QuadraticTriangle(vids)
-#    elseif Δ == 8
-#        vids = conn[Vec(ntuple(i -> i + offset - 1, Val(8))...)]
-#        return QuadraticQuadrilateral(vids)
-#    else
-#        error("Unsupported type.")
-#        return nothing
-#    end
-#end
-#
-#function materialize_faces(mesh::VolumeMesh)
-#    return map(i -> materialize_face(i, mesh), 1:nelements(mesh))
-#end
-#
-#
-#
-#
-#
-#
 #
 #function materialize_polytope(i::Integer, mesh::PolytopeVertexMesh)
 #    return materialize(mesh.polytopes[i], mesh.vertices)
@@ -123,57 +74,3 @@ end
 ##            )   
 ##end
 ##
-### TODO:
-### Fast faces methods for Polyhedrons/quadratic polyhedrons where all elements are the same type
-##
-### All edges are line segments
-##function materialize_edges(mesh::PolytopeVertexMesh{D,T,P}) where {D,T,P<:Polygon}
-##    unique_edges = Vec{2,vertex_type(P)}[]
-##    nedges = 0
-##    for face ∈ mesh.polytopes
-##        edge_vecs = edges(face)
-##        for edge ∈ edge_vecs
-##            if edge[1] < edge[2]
-##                sorted_edge = edge.vertices
-##            else
-##                sorted_edge = Vec(edge[2], edge[1])
-##            end
-##            index = getsortedfirst(unique_edges, sorted_edge)
-##            if nedges < index || unique_edges[index] !== sorted_edge
-##                insert!(unique_edges, index, sorted_edge)
-##                nedges += 1
-##            end
-##        end
-##    end
-##    materialized_edges = Vector{LineSegment{Point{D,T}}}(undef, nedges)
-##    for i = 1:nedges
-##        materialized_edges[i] = materialize(LineSegment(unique_edges[i]), mesh.vertices)
-##    end
-##    return materialized_edges 
-##end 
-##
-### All edges are quadratic segments
-##function materialize_edges(mesh::PolytopeVertexMesh{D,T,P}) where {D,T,P<:QuadraticPolygon}
-##    unique_edges = Vec{3,vertex_type(P)}[]
-##    nedges = 0
-##    for face ∈ mesh.polytopes
-##        edge_vecs = edges(face)
-##        for edge ∈ edge_vecs
-##            if edge[1] < edge[2]
-##                sorted_edge = edge.vertices
-##            else
-##                sorted_edge = Vec(edge[2], edge[1], edge[3])
-##            end
-##            index = getsortedfirst(unique_edges, sorted_edge)
-##            if nedges < index || unique_edges[index] !== sorted_edge
-##                insert!(unique_edges, index, sorted_edge)
-##                nedges += 1
-##            end
-##        end
-##    end
-##    materialized_edges = Vector{QuadraticSegment{Point{D,T}}}(undef, nedges)
-##    for i = 1:nedges
-##        materialized_edges[i] = materialize(QuadraticSegment(unique_edges[i]), mesh.vertices)
-##    end
-##    return materialized_edges 
-##end 
