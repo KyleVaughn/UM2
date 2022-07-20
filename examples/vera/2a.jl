77cdcf572ea78d9a4cce8373c4c38b7a33fedb48
Kyle Vaughn <kcvaughn@umich.edu>
2022-07-20 12:29:41 -0400

    2a update

--- examples/vera/2a.jl
+++ examples/vera/2a.jl
@@ -2,7 +2,7 @@
 # Revision 4, August 29, 2014
 # CASL-U-2012-0131-004
 using UM2
-
+file_prefix = "2a"
 add_timestamps_to_logger()
 gmsh.initialize()
 gmsh.option.set_number("General.NumThreads", 0) # 0 uses system default, i.e. OMP_NUM_THREADS)
@@ -11,10 +11,10 @@ gmsh.option.set_number("General.Verbosity", 2) # 1: +errors, 2: +warnings, 3: +d
 
 # Model
 # ----------------------------------------------------------------------------------------------
-uo2_entities = Int64[];
+fuel_entities = Int64[];
 gap_entities = Int64[];
 clad_entities = Int64[];
-h2o_entities = Int64[];
+water_entities = Int64[];
 r_fuel = 0.4096 # Pellet radius = 0.4096 cm (pg. 4)
 r_gap = 0.418   # Inner clad radius = 0.418 cm (pg.4)
 r_clad = 0.475  # Outer clad radius = 0.475 cm (pg.4)
@@ -37,7 +37,7 @@ for i in 1:17, j in 1:17
     end
     x = half_gap + i * pitch - pitch / 2
     y = half_gap + j * pitch - pitch / 2
-    push!(uo2_entities, gmsh.model.occ.add_disk(x, y, 0, r_fuel, r_fuel))
+    push!(fuel_entities, gmsh.model.occ.add_disk(x, y, 0, r_fuel, r_fuel))
     push!(gap_entities, gmsh.model.occ.add_disk(x, y, 0, r_gap, r_gap))
     push!(clad_entities, gmsh.model.occ.add_disk(x, y, 0, r_clad, r_clad))
 end
@@ -48,7 +48,7 @@ r_gt_outer = 0.602 # Outer Guide Tube Radius = 0.602 cm (pg. 5)
 for (i, j) in coords_gt
     x = half_gap + i * pitch - pitch / 2
     y = half_gap + j * pitch - pitch / 2
-    push!(h2o_entities, gmsh.model.occ.add_disk(x, y, 0, r_gt_inner, r_gt_inner))
+    push!(water_entities, gmsh.model.occ.add_disk(x, y, 0, r_gt_inner, r_gt_inner))
     push!(clad_entities, gmsh.model.occ.add_disk(x, y, 0, r_gt_outer, r_gt_outer))
 end
 
@@ -58,7 +58,7 @@ r_it_outer = 0.605 # Outer Instrument Tube Radius = 0.605 cm (pg. 5)
 for (i, j) in coords_it
     x = half_gap + i * pitch - pitch / 2
     y = half_gap + j * pitch - pitch / 2
-    push!(h2o_entities, gmsh.model.occ.add_disk(x, y, 0, r_it_inner, r_it_inner))
+    push!(water_entities, gmsh.model.occ.add_disk(x, y, 0, r_it_inner, r_it_inner))
     push!(clad_entities, gmsh.model.occ.add_disk(x, y, 0, r_it_outer, r_it_outer))
 end
 gmsh.model.occ.synchronize()
@@ -66,16 +66,16 @@ gmsh.model.occ.synchronize()
 # Materials
 # ----------------------------------------------------------------------------------------------
 # http://juliagraphics.github.io/Colors.jl/stable/namedcolors/
-materials = Material[Material(name = "UO2", color = "forestgreen"),
+materials = Material[Material(name = "Fuel", color = "forestgreen"),
                      Material(name = "Gap", color = "darkorange"),
-                     Material(name = "H2O", color = "royalblue"),
+                     Material(name = "Water", color = "royalblue"),
                      Material(name = "Clad", color = "slategrey")]
-safe_add_physical_group("Material: UO2", [(2, i) for i in uo2_entities])
+safe_add_physical_group("Material: Fuel", [(2, i) for i in fuel_entities])
 safe_add_physical_group("Material: Gap", [(2, i) for i in gap_entities])
-safe_add_physical_group("Material: H2O", [(2, i) for i in h2o_entities])
+safe_add_physical_group("Material: Water", [(2, i) for i in water_entities])
 safe_add_physical_group("Material: Clad", [(2, i) for i in clad_entities])
 ents = gmsh.model.get_entities(2)
-# Fragment the many disks, prioritizing UO2 > Gap > H2O > Clad to fill space
+# Fragment the many disks, prioritizing Fuel > Gap > Water > Clad to fill space
 safe_fragment(ents, ents, material_hierarchy = materials)
 
 # Overlay Grid
@@ -86,20 +86,20 @@ push!(coarse_divs, 21.5) # Assembly pitch = 21.50 cm (pg. 7)
 scoarse_divs = Vec{18, Float64}(coarse_divs)
 coarse_grid = RectilinearGrid(scoarse_divs, scoarse_divs)
 mpact_grid = MPACTGridHierarchy(coarse_grid)
-# We now want H2O to fill empty space, preserving all other materials,
-# so we need to swap H2O to the bottom of the materials hierarchy
+# We now want Water to fill empty space, preserving all other materials,
+# so we need to swap Water to the bottom of the materials hierarchy
 materials[3], materials[4] = materials[4], materials[3]
 overlay_mpact_grid_hierarchy(mpact_grid, materials)
 
 # Mesh
 # ------------------------------------------------------------------------------------------------
-lc = 0.25 # cm
+lc = pitch/5 # cm
 for mat in materials
-    mat.mesh_size = lc
+    mat.lc = lc
 end
 set_mesh_field_using_materials(materials)
 generate_mesh(order = 1, faces = "Triangle", opt_iters = 2)
-gmsh.write("2a.inp")
+gmsh.write(file_prefix*".inp")
 mesh_error = get_cad_to_mesh_error()
 for i in eachindex(mesh_error)
     println(mesh_error[i])
@@ -107,8 +107,8 @@ end
 #gmsh.fltk.run()
 gmsh.finalize()
 
-mesh = import_mesh("2a.inp")
+mesh = import_mesh(file_prefix*".inp")
 #statistics(mesh)
 # Partition mesh according to mpact grid hierarchy and write as an xdmf file 
 mpt = MeshPartitionTree(mesh)
-export_mesh(mpt, "2a.xdmf")
+export_mesh(file_prefix*".xdmf", mpt)
