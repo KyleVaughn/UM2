f97c684adf89f5a4ebbd21a20ce6c40fbfa0cd5f
Kyle Vaughn <kcvaughn@umich.edu>
2022-07-26 23:31:42 -0400

    Update + plot with colors for mesh

--- src/plot/volume_mesh.jl
+++ src/plot/volume_mesh.jl
@@ -6,6 +6,11 @@ function convert_arguments(LS::Type{<:LineSegments}, mesh::VolumeMesh{2})
     return convert_arguments(LS, materialize_edges(mesh))
 end
 
-function convert_arguments(M::Type{<:GLMakieMesh}, mesh::VolumeMesh{2})
-    return convert_arguments(M, materialize_faces(mesh))
+function mesh!(mesh::VolumeMesh{2})
+    mesh_faces = materialize_faces(mesh)
+    unique_mats = unique(mesh.materials)
+    for mat in unique_mats
+        mesh!(mesh_faces[map(m->m == mat, mesh.materials)])
+    end
+    return nothing
 end
