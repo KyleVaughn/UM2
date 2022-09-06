5172183116b6280973fb9589b846e2474840ff41
Kyle Vaughn <kcvaughn@umich.edu>
2022-09-06 15:10:15 -0400

    Tuning size

--- src/math/hilbert.jl
+++ src/math/hilbert.jl
@@ -1,16 +1,18 @@
+const HILBERT_SIZE = 2^10
+
 function encode_hilbert(x::T, y::T, s_inv::T) where {T <: AbstractFloat}
     # Assumes x and y are in the range [0, s]
     # s_inv is the inverse of the side length of the square
     # x and y are the coordinates of the point
 
     # Convert to UInt16
-    x_u16 = floor(UInt16, x * s_inv * 100)
-    y_u16 = floor(UInt16, y * s_inv * 100)
+    x_u16 = floor(Int16, x * s_inv * HILBERT_SIZE)
+    y_u16 = floor(Int16, y * s_inv * HILBERT_SIZE)
     return encode_hilbert(x_u16, y_u16)
 end
 
 function encode_hilbert(x::T, y::T) where {T <: Integer}
-    n = T(2^15)
+    n = T(HILBERT_SIZE)
     # Assumes x and y are in the range [0, n - 1]
     d = T(0)
     s = n >> T(1)
