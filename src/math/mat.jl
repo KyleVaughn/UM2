70a3f7e001e55a8753a6c80bb2fe1ee8ac6823c8
Kyle Vaughn <kcvaughn@umich.edu>
2022-09-07 13:47:32 -0400

    Update?

--- src/math/mat.jl
+++ src/math/mat.jl
@@ -40,9 +40,9 @@ Base.:-(A::Mat) = Mat(map(-, A.cols))
 
 # -- Binary operators --
 
-Base.:*(A::Mat{M, N, T}, scalar::X) where {M, N, T, X <: Number} = Mat(map(x -> x * T(scalar), A.cols))
-Base.:/(A::Mat{M, N, T}, scalar::X) where {M, N, T, X <: Number} = Mat(map(x -> x / T(scalar), A.cols))
 Base.:*(scalar::X, A::Mat{M, N, T}) where {M, N, T, X <: Number} = Mat(map(x -> T(scalar) * x, A.cols))
+Base.:*(A::Mat{M, N, T}, scalar::X) where {M, N, T, X <: Number} = scalar * A
+Base.:/(A::Mat{M, N, T}, scalar::X) where {M, N, T, X <: Number} = Mat(map(x -> x / T(scalar), A.cols))
 Base.:+(A::Mat{M, N, T}, B::Mat{M, N, T}) where {M, N, T} = Mat(map(+, A.cols, B.cols))
 Base.:-(A::Mat{M, N, T}, B::Mat{M, N, T}) where {M, N, T} = Mat(map(-, A.cols, B.cols))
 
@@ -53,26 +53,26 @@ function Mat2x2(a11::T, a21::T, a12::T, a22::T) where {T}
 end
 
 function Base.:*(A::Mat2x2, x::Vec2)
-    return Vec2(A[1,1] * x[1] + A[1,2] * x[2],
-                A[2,1] * x[1] + A[2,2] * x[2])
+    return Vec2(A[1, 1] * x[1] + A[1, 2] * x[2],
+                A[2, 1] * x[1] + A[2, 2] * x[2])
 end
 
 function Base.:*(A::Mat2x2, B::Mat2x2)
-    return Mat2x2(A[1,1] * B[1,1] + A[1,2] * B[2,1],
-                  A[2,1] * B[1,1] + A[2,2] * B[2,1],
-                  A[1,1] * B[1,2] + A[1,2] * B[2,2],
-                  A[2,1] * B[1,2] + A[2,2] * B[2,2])
+    return Mat2x2(A[1, 1] * B[1, 1] + A[1, 2] * B[2, 1],
+                  A[2, 1] * B[1, 1] + A[2, 2] * B[2, 1],
+                  A[1, 1] * B[1, 2] + A[1, 2] * B[2, 2],
+                  A[2, 1] * B[1, 2] + A[2, 2] * B[2, 2])
 end
 
 function det(A::Mat2x2)
-    return A[1,1] * A[2,2] - A[2,1] * A[1,2] 
+    return A[1, 1] * A[2, 2] - A[2, 1] * A[1, 2] 
 end
 
 function Base.inv(A::Mat2x2)
-    return Mat2x2( A[2,2], 
-                  -A[2,1],
-                  -A[1,2],  
-                   A[1,1]) / det(A)
+    return Mat2x2( A[2, 2], 
+                  -A[2, 1],
+                  -A[1, 2],  
+                   A[1, 1]) / det(A)
 end
 
 # -- IO --
