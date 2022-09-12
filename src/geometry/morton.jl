061409a1398ba4582ad16ad16362b878ae1e55dc
Kyle Vaughn <kcvaughn@umich.edu>
2022-09-11 22:29:13 -0400

    working on nearest neighbor with morton

--- src/geometry/morton.jl
+++ src/geometry/morton.jl
@@ -1,74 +1,200 @@
 export encode_morton, find_nearest_point
 
-function encode_morton(p::Point2{T}, s_inv::T) where {T}    
-    return encode_morton(p[1], p[2], s_inv)    
+function encode_morton(p::Point2{T}, scale_inv::T) where {T}    
+    return encode_morton(p[1], p[2], scale_inv)    
 end  
 
-# annoying needed for anonymous functions
-encode_morton(x::UInt32, s_inv::Float32) = x
-encode_morton(x::UInt32, s_inv::Float64) = x
+# Needed for sort by anonymous function
+function encode_morton(uint::U, ::T) where {U <: Unsigned, T <: AbstractFloat}
+    return uint
+end
 
+
+# Traverse the concentric 
 function find_nearest_point(
         p::Point2{T}, 
-        s_inv::T, 
+        scale_inv::T, 
         points::Vector{Point2{T}}) where {T}
     # Points sorted in z-order are sorted into an implicit quadtree.
     # The nearest point is in the same quadtree node or one of its neighbors.
-    # WE ASSUME Z IS 32 BITS and search through the quadtree
     # 
     # ^  2  3     ^  10  11
     # |           |  
     # y  0  1     y  00  01
     #   x -->       x -->
     # 
-    # Append 2 bits to the z-order code each level of the tree until we find only one point.
+    # Append 2 bits to the z-order code each level of the tree until 
+    # we run out of bits or we run out of points.
     # The zeroth quadtree node: 00
     # The first quadtree node:  01
     # The second quadtree node: 10
     # The third quadtree node:  11
-    z::UInt32 = encode_morton(p, s_inv)
-    z_lo = 0x00000000
-    z_hi = 0xffffffff
-    z_lo_old = z_lo
-    z_hi_old = z_hi
-    # z_0 = 0b00000000000000000000000000000000 
-    z_1 = 0b01000000000000000000000000000000
-    z_2 = 0b10000000000000000000000000000000
-    # z_3 = 0b11000000000000000000000000000000
+    s = MORTON_MAX_SIDE 
+    z = encode_morton(p, scale_inv)
+    lo = 1
     N = length(points)
-    i_lo = 1
-    i_hi = N 
-    for _ in 1:16 # 4^16 = 2^32
-        # Find the quadtree node containing z.
-        if z < z_lo + z_2 # bottom half 
-            z_hi -= z_2
-        else # top half
-            z_lo += z_2
+    hi = N
+    fixed_lo = false
+    fixed_hi = false
+    z_search = typemin(MORTON_INDEX_TYPE)
+    z_cut = (typemax(MORTON_INDEX_TYPE) >> 1) + 1 # 0x800...000
+    all_ones = typemax(MORTON_INDEX_TYPE)
+    uzero = zero(MORTON_INDEX_TYPE)
+    for l in 1:MORTON_MAX_LEVELS
+        fy = z & z_cut; z_cut >>= 1
+        fx = z & z_cut; z_cut >>= 1
+        z_search |= (fx | fy)
+        if (fx !== uzero || fy !== uzero) && !fixed_lo
+            # update lo
+            view_lo = searchsortedlast(view(points, lo:hi), z_search, 
+                                        by=p->encode_morton(p, scale_inv))
+            new_lo = lo + view_lo - 1 
+            if lo < new_lo && new_lo < hi
+                lo = new_lo
+            else
+                fixed_lo = true
+            end
         end
-        if z < z_lo + z_1 # left half
-            z_hi -= z_1
-        else # right half
-            z_lo += z_1
+        if (fx === uzero || fy === uzero) && !fixed_hi
+            # update hi
+            # max morton index is obtained by flipping all lower bits to 1
+            z_max = z_search | (all_ones >> (l * 2))
+            view_hi = searchsortedfirst(view(points, lo:hi), z_max, 
+                                       by=p->encode_morton(p, scale_inv))
+            new_hi = lo + view_hi - 1 
+            if lo < new_hi && new_hi < hi
+                hi = new_hi
+            else
+                fixed_hi = true
+            end
         end
-        # Find the lower and upper bound on the indices of the points in the points vector
-        # that are in the quadtree node.
-        if z_hi !== z_hi_old
-            i_hi = searchsortedlast(view(points, i_lo:i_hi), z_hi, by = p->encode_morton(p, s_inv))
-            z_hi_old = z_hi
+
+        z_max = z_search | (all_ones >> (l * 2))
+        println(lo, " ", hi, " ", z_search, " ",z, " ", z_max)
+        println(encode_morton(points[lo], scale_inv), " ", encode_morton(points[hi], scale_inv))
+        println(fixed_lo, " ", fixed_hi)
+        if fixed_lo && fixed_hi
+            break
         end
-        if z_lo !== z_lo_old
-            i_lo = searchsortedfirst(view(points, i_lo:i_hi), z_lo, by = p->encode_morton(p, s_inv))
-            z_lo_old = z_lo
-        end
-        # If the quadtree node contains only one point, return it.
-#        if i_lo === i_hi
-#            return points[i_lo]
-#        end
-        # If the quadtree node contains more than one point, continue searching.
-        # z_0 >>= 2 
-        z_1 >>= 2
-        z_2 >>= 2
-        #z_3 >>= 2
+        linesegments!(LineSegment(points[lo], points[hi]))
+        readline()
+        pmin = Point2d(decode_morton(z_search, 1/scale_inv))
+        pmax = Point2d(decode_morton(z_max, 1/scale_inv))
+        linesegments!(AABB(pmin, pmax))
+        readline()
     end
-    error("Failed to find nearest point.")
+    # Find the nearest point in the range [lo, hi]
+    nearest = lo
+    nearest_dist = distance2(p, points[lo])
+    for i in lo+1:hi
+        dist = distance2(p, points[i])
+        if dist < nearest_dist
+            nearest = i
+            nearest_dist = dist
+        end
+    end
+    return nearest
 end
+
+
+
+
+
+#        #z_3 >>= 2
+#    end
+#    error("Failed to find nearest point.")
+#end
+#
+#function find_nearest_point(
+#        p::Point2{T}, 
+#        scale_inv::T, 
+#        points::Vector{Point2{T}}) where {T}
+#    # Points sorted in z-order are sorted into an implicit quadtree.
+#    # The nearest point is in the same quadtree node or one of its neighbors.
+#    # 
+#    # ^  2  3     ^  10  11
+#    # |           |  
+#    # y  0  1     y  00  01
+#    #   x -->       x -->
+#    # 
+#    # Append 2 bits to the z-order code each level of the tree until 
+#    # we run out of bits or we run out of points.
+#    # The zeroth quadtree node: 00
+#    # The first quadtree node:  01
+#    # The second quadtree node: 10
+#    # The third quadtree node:  11
+#    s = MORTON_MAX_SIDE 
+#    z = encode_morton(p, scale_inv)
+#    lo = 1
+#    N = length(points)
+#    hi = N
+#    fixed_lo = false
+#    fixed_hi = false
+#    z_search = typemin(MORTON_INDEX_TYPE)
+#    z_cut = (typemax(MORTON_INDEX_TYPE) >> 1) + 1 # 0x800...000
+#    all_ones = typemax(MORTON_INDEX_TYPE)
+#    uzero = zero(MORTON_INDEX_TYPE)
+#    for l in 1:MORTON_MAX_LEVELS
+#        fy = z & z_cut; z_cut >>= 1
+#        fx = z & z_cut; z_cut >>= 1
+#        z_search |= (fx | fy)
+#        if (fx !== uzero || fy !== uzero) && !fixed_lo
+#            # update lo
+#            view_lo = searchsortedlast(view(points, lo:hi), z_search, 
+#                                        by=p->encode_morton(p, scale_inv))
+#            new_lo = lo + view_lo - 1 
+#            if lo < new_lo && new_lo < hi
+#                lo = new_lo
+#            else
+#                fixed_lo = true
+#            end
+#        end
+#        if (fx === uzero || fy === uzero) && !fixed_hi
+#            # update hi
+#            # max morton index is obtained by flipping all lower bits to 1
+#            z_max = z_search | (all_ones >> (l * 2))
+#            view_hi = searchsortedfirst(view(points, lo:hi), z_max, 
+#                                       by=p->encode_morton(p, scale_inv))
+#            new_hi = lo + view_hi - 1 
+#            if lo < new_hi && new_hi < hi
+#                hi = new_hi
+#            else
+#                fixed_hi = true
+#            end
+#        end
+#
+#        z_max = z_search | (all_ones >> (l * 2))
+#        println(lo, " ", hi, " ", z_search, " ",z, " ", z_max)
+#        println(encode_morton(points[lo], scale_inv), " ", encode_morton(points[hi], scale_inv))
+#        println(fixed_lo, " ", fixed_hi)
+#        if fixed_lo && fixed_hi
+#            break
+#        end
+#        linesegments!(LineSegment(points[lo], points[hi]))
+#        readline()
+#        pmin = Point2d(decode_morton(z_search, 1/scale_inv))
+#        pmax = Point2d(decode_morton(z_max, 1/scale_inv))
+#        linesegments!(AABB(pmin, pmax))
+#        readline()
+#    end
+#    # Find the nearest point in the range [lo, hi]
+#    nearest = lo
+#    nearest_dist = distance2(p, points[lo])
+#    for i in lo+1:hi
+#        dist = distance2(p, points[i])
+#        if dist < nearest_dist
+#            nearest = i
+#            nearest_dist = dist
+#        end
+#    end
+#    return nearest
+#end
+#
+#
+#
+#
+#
+##        #z_3 >>= 2
+##    end
+##    error("Failed to find nearest point.")
+##end
--- src/math/morton.jl
+++ src/math/morton.jl
@@ -1,73 +1,119 @@
-export encode_morton
+export encode_morton, decode_morton
 
+const MORTON_INDEX_TYPE = UInt64 # 32-bit or 64-bit
+const MORTON_MAX_LEVELS = 4 * sizeof(MORTON_INDEX_TYPE)
+const MORTON_MAX_SIDE  = one(MORTON_INDEX_TYPE) << MORTON_MAX_LEVELS
+const MORTON_MAX_INDEX = typemax(MORTON_INDEX_TYPE)
 
-const MAX_MORTON_INDEX = 0x000000000000ffff # 2^16 - 1
+const morton_masks_32 = (
+    0x0000ffff, 
+    0x00ff00ff,
+    0x0f0f0f0f,
+    0x33333333,
+    0x55555555,
+)
 
-# Normalizes the coordinate to the range [0, 65535] (2^16 - 1)
-# This guarantees that the Morton code will fit in 32 bits, since
-# (2^16 - 1)^2 < 2^32 - 1
-function encode_morton(x::T, y::T, s_inv::T) where {T <: Union{Float32, Float64}}
-    x_u32 = floor(UInt32, x * s_inv * MAX_MORTON_INDEX)
-    y_u32 = floor(UInt32, y * s_inv * MAX_MORTON_INDEX)
-    return encode_morton(x_u32, y_u32)
+const morton_masks_64 = (
+    0x00000000ffffffff,
+    0x0000ffff0000ffff,
+    0x00ff00ff00ff00ff,
+    0x0f0f0f0f0f0f0f0f,
+    0x3333333333333333,
+    0x5555555555555555,
+)     
+
+function pdep_0x55555555(x::UInt32)
+    x &= morton_masks_32[1]
+    x = (x | (x << 8)) & morton_masks_32[2]
+    x = (x | (x << 4)) & morton_masks_32[3]
+    x = (x | (x << 2)) & morton_masks_32[4]
+    x = (x | (x << 1)) & morton_masks_32[5]
+    return x
 end
 
-# This doesn't produce a good correlation between space and morton index.
-## Encodes the coordinates into a Morton number by
-## reinterpretting the bits as a 32-bit unsigned integer.
-#function encode_morton(x::Float32, y::Float32)
-#    x_u64 = widen(reinterpret(UInt32, x))
-#    y_u64 = widen(reinterpret(UInt32, y))
-#    return encode_morton(x_u64, y_u64)
-#end
-#
-#function encode_morton(x::Float64, y::Float64)
-#    # Convert to a 32-bit float first.
-#    x_u64 = widen(reinterpret(UInt32, Float32(x)))
-#    y_u64 = widen(reinterpret(UInt32, Float32(y)))
-#    return encode_morton(x_u64, y_u64)
-#end
+function pdep_0x5555555555555555(x::UInt64)
+    x &= morton_masks_64[1]
+    x = (x | (x << 16)) & morton_masks_64[2]
+    x = (x | (x <<  8)) & morton_masks_64[3]
+    x = (x | (x <<  4)) & morton_masks_64[4]
+    x = (x | (x <<  2)) & morton_masks_64[5]
+    x = (x | (x <<  1)) & morton_masks_64[6]
+    return x
+end
 
-# For UInt32, assumes x, y are in the range [0, 2^16)
-# For UInt64, assumes x, y are in the range [0, 2^32)
+function pext_0x55555555(x::UInt32)
+    x &= morton_masks_32[5]
+    x = (x ^ (x >> 1)) & morton_masks_32[4]
+    x = (x ^ (x >> 2)) & morton_masks_32[3]
+    x = (x ^ (x >> 4)) & morton_masks_32[2]
+    x = (x ^ (x >> 8)) & morton_masks_32[1]
+    return x
+end
+
+function pext_0x5555555555555555(x::UInt64)
+    x &= morton_masks_64[6]
+    x = (x ^ (x >>  1)) & morton_masks_64[5]
+    x = (x ^ (x >>  2)) & morton_masks_64[4]
+    x = (x ^ (x >>  4)) & morton_masks_64[3]
+    x = (x ^ (x >>  8)) & morton_masks_64[2]
+    x = (x ^ (x >> 16)) & morton_masks_64[1]
+    return x
+end
+
+# For UInt32, assumes x, y are in the range [0, 2^16]
+# For UInt64, assumes x, y are in the range [0, 2^32]
 if UM2_HAS_BMI2
     function encode_morton(x::UInt32, y::UInt32)
+        if x == MORTON_MAX_SIDE && y == MORTON_MAX_SIDE
+            return UInt32(MORTON_MAX_INDEX)
+        end
         return pdep(x, 0x55555555) | pdep(y, 0xaaaaaaaa)
     end
+
     function encode_morton(x::UInt64, y::UInt64)
+        if x == MORTON_MAX_SIDE && y == MORTON_MAX_SIDE
+            return UInt64(MORTON_MAX_INDEX)
+        end
         return pdep(x, 0x5555555555555555) | pdep(y, 0xaaaaaaaaaaaaaaaa)
     end
+
+    decode_morton(z::UInt32) = pext(z, 0x55555555), pext(z, 0xaaaaaaaa)
+    decode_morton(z::UInt64) = pext(z, 0x5555555555555555), pext(z, 0xaaaaaaaaaaaaaaaa)
 else
     function encode_morton(x::UInt32, y::UInt32)
-        x = (x | (x << 16)) & 0x0000ffff
-        x = (x | (x << 8))  & 0x00ff00ff
-        x = (x | (x << 4))  & 0x0f0f0f0f
-        x = (x | (x << 2))  & 0x33333333
-        x = (x | (x << 1))  & 0x55555555
-    
-        y = (y | (y << 16)) & 0x0000ffff
-        y = (y | (y << 8))  & 0x00ff00ff
-        y = (y | (y << 4))  & 0x0f0f0f0f
-        y = (y | (y << 2))  & 0x33333333
-        y = (y | (y << 1))  & 0x55555555
+        x = pdep_0x55555555(x)
+        y = pdep_0x55555555(y)
+        return x | (y << 1)
+    end
 
-        return x | (y << 1)
-    end
     function encode_morton(x::UInt64, y::UInt64)
-        x = (x | (x << 32)) & 0x00000000ffffffff
-        x = (x | (x << 16)) & 0x0000ffff0000ffff
-        x = (x | (x << 8))  & 0x00ff00ff00ff00ff
-        x = (x | (x << 4))  & 0x0f0f0f0f0f0f0f0f
-        x = (x | (x << 2))  & 0x3333333333333333
-        x = (x | (x << 1))  & 0x5555555555555555
-    
-        y = (y | (y << 32)) & 0x00000000ffffffff
-        y = (y | (y << 16)) & 0x0000ffff0000ffff
-        y = (y | (y << 8))  & 0x00ff00ff00ff00ff
-        y = (y | (y << 4))  & 0x0f0f0f0f0f0f0f0f
-        y = (y | (y << 2))  & 0x3333333333333333
-        y = (y | (y << 1))  & 0x5555555555555555
-    
+        x = pdep_0x5555555555555555(x)
+        y = pdep_0x5555555555555555(y)
         return x | (y << 1)
     end
+
+    function decode_morton(z::UInt32)
+        x = pext_0x55555555(z)
+        y = pext_0x55555555(z >> 1)
+        return x, y
+    end
+
+    function decode_morton(z::UInt64)
+        x = pext_0x5555555555555555(z)
+        y = pext_0x5555555555555555(z >> 1)
+        return x, y
+    end
+end
+
+# Normalizes the coordinate to the range [0, _MORTON_MAX_SIDE]
+function encode_morton(x::T, y::T, scale_inv::T) where {T <: Union{Float32, Float64}}
+    x_u = floor(MORTON_INDEX_TYPE, x * scale_inv * MORTON_MAX_SIDE )
+    y_u = floor(MORTON_INDEX_TYPE, y * scale_inv * MORTON_MAX_SIDE )
+    return encode_morton(x_u, y_u)
+end
+
+function decode_morton(z::MORTON_INDEX_TYPE, scale::T
+    ) where {T <: Union{Float32, Float64}}
+    x, y = decode_morton(z)
+    return x * scale / MORTON_MAX_SIDE, y * scale / MORTON_MAX_SIDE
 end
