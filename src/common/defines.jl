8486410335e26d39faa87b9d4b5d4f75ad2f4c56
Kyle Vaughn <kcvaughn@umich.edu>
2022-10-11 19:57:26 -0400

    Generalized instruction set determination

--- src/common/defines.jl
+++ src/common/defines.jl
@@ -1,7 +1,12 @@
+# The default floating point type used by the library
 const UM_FLOAT = Float64
+# The default integer type used by the library
 const UM_INT = Int32
 
+# Alias for the default floating point type
 const UM_F = UM_FLOAT
+# Alias for the default integer type
 const UM_I = UM_INT
 
+# If this
 const UM_HAS_BMI2 = true
--- src/common/instructions.jl
+++ src/common/instructions.jl
@@ -1,3 +1,11 @@
+# Run CPUID with EAX = 7, ECX = 0 and return the result in a tuple
+# (eax, ebx, ecx, edx)
+# https://en.wikipedia.org/wiki/CPUID#EAX=7,_ECX=0:_Extended_Features
+CPUInfo = zeros(Int32, 4)
+ccall(:jl_cpuidex, Cvoid, (Ptr{Cint}, Cint, Cint), CPUInfo, 7, 0)
+# if bit 8 of ebx is set, BMI2 is supported
+const UM_HAS_BMI2 = CPUInfo[2] & 0x100 != 0
+
 if UM_HAS_BMI2
     @inline function pdep(x::UInt32, y::UInt32)
         return ccall("llvm.x86.bmi.pdep.32", llvmcall, UInt32, (UInt32, UInt32), x, y)
