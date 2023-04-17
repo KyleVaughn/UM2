d6de7a3786cc1446bf51f4118ef0f1bf3db891b4
Kyle Vaughn <kcvaughn@umich.edu>
2023-04-17 02:12:48 -0400

    WIP: clang format/tidy

--- src/um2c.cpp
+++ src/um2c.cpp
@@ -10,7 +10,7 @@ void um2_malloc(void ** const p, int const size)
 
 void um2_free(void * const p)
 {
-    if (p) free(p);
+    if (p != nullptr) { free(p); }
 }
 
 void um2_sort(double * begin, double * end)
