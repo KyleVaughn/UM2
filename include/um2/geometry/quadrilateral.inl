9506416d50c83fca2db613207d4fe35021095875
isaachyw <isaachyw@umich.edu>
2023-06-08 12:52:39 -0400

    quad & spack dependency

---
+++ include/um2/geometry/quadrilateral.inl
@@ -0,0 +1,93 @@
+namespace um2
+{
+
+// -------------------------------------------------------------------
+// Accessors
+// -------------------------------------------------------------------
+
+template <len_t D, typename T>
+UM2_NDEBUG_PURE UM2_HOSTDEV constexpr auto Quadrilateral<D, T>::operator[](len_t i)
+    -> Point<D, T> &
+{
+  return this->vertices[i];
+}
+
+template <len_t D, typename T>
+UM2_NDEBUG_PURE UM2_HOSTDEV constexpr auto Quadrilateral<D, T>::operator[](len_t i) const
+    -> Point<D, T> const &
+{
+  return this->vertices[i];
+}
+
+// -------------------------------------------------------------------
+// Interpolation
+// -------------------------------------------------------------------
+
+template <len_t D, typename T>
+template <typename R, typename S>
+UM2_PURE UM2_HOSTDEV constexpr auto
+Quadrilateral<D, T>::operator()(R const r, S const s) const noexcept -> Point<D, T>
+{
+  T const rr = static_cast<T>(r);
+  T const ss = static_cast<T>(s);
+  T const w[4] = {(1 - rr) * (1 - ss), rr * (1 - ss), rr * ss, (1 - rr) * ss};
+  return w[0] * this->vertices[0] + w[1] * this->vertices[1] + w[2] * this->vertices[2] +
+         w[3] * this->vertices[3];
+}
+
+// -------------------------------------------------------------------
+// jacobian
+// -------------------------------------------------------------------
+
+template <len_t D, typename T>
+template <typename R, typename S>
+UM2_PURE UM2_HOSTDEV constexpr auto Quadrilateral<D, T>::jacobian(R r, S s) const noexcept
+    -> Mat<D, 2, T>
+{
+  T const rr = static_cast<T>(r);
+  T const ss = static_cast<T>(s);
+  Mat<D, 2, T> jac;
+  jac.col(0) = (1 - ss) * (this->vertices[1] - this->vertices[0]) +
+               (-ss) * (this->vertices[3] - this->vertices[2]);
+  jac.col(1) = (1 - rr) * (this->vertices[3] - this->vertices[0]) +
+               (-rr) * (this->vertices[1] - this->vertices[2]);
+  return jac;
+}
+
+// -------------------------------------------------------------------
+// edge
+// -------------------------------------------------------------------
+
+template <len_t D, typename T>
+UM2_PURE UM2_HOSTDEV constexpr auto Quadrilateral<D, T>::edge(len_t i) const noexcept
+    -> LineSegment<D, T>
+{
+  if (i != 3) {
+    return {this->vertices[i], this->vertices[i + 1]};
+  }
+  return {this->vertices[i], this->vertices[0]};
+}
+
+// -------------------------------------------------------------------
+// contains
+// -------------------------------------------------------------------
+
+template <len_t D, typename T>
+UM2_PURE UM2_HOSTDEV constexpr auto
+Quadrilateral<D, T>::contains(Point<D, T> const & p) const noexcept -> bool
+  requires(D == 2)
+{
+  return areCCW(this->vertices[0], this->vertices[1], p) &&
+         areCCW(this->vertices[1], this->vertices[2], p) &&
+         areCCW(this->vertices[2], this->vertices[3], p) &&
+         areCCW(this->vertices[3], this->vertices[0], p);
+}
+
+// -------------------------------------------------------------------
+// See polytope.inl for
+// -------------------------------------------------------------------
+// area
+// centroid
+// boundingBox
+
+} // namespace um2
---
+++ tests/geometry/quadrilateral.cpp
@@ -0,0 +1,187 @@
+#include "../test_framework.hpp"
+#include <um2/geometry/quadrilateral.hpp>
+
+template <len_t D, typename T>
+UM2_HOSTDEV static constexpr auto makeQuad() -> um2::Quadrilateral<D, T>
+{
+  um2::Quadrilateral<D, T> quad;
+  for (len_t i = 0; i < 4; ++i) {
+    for (len_t j = 0; j < D; ++j) {
+      quad[i][j] = static_cast<T>(0);
+    }
+  }
+  quad[1][0] = static_cast<T>(1);
+  quad[2][0] = static_cast<T>(1);
+  quad[2][1] = static_cast<T>(1);
+  quad[3][1] = static_cast<T>(1);
+  ;
+  return quad;
+}
+
+// -------------------------------------------------------------------
+// Interpolation
+// -------------------------------------------------------------------
+
+template <len_t D, typename T>
+UM2_HOSTDEV TEST_CASE(interpolate)
+{
+  um2::Quadrilateral<D, T> quad = makeQuad<D, T>();
+  um2::Point<D, T> p00 = quad(0, 0);
+  um2::Point<D, T> p10 = quad(1, 0);
+  um2::Point<D, T> p01 = quad(0, 1);
+  um2::Point<D, T> p11 = quad(1, 1);
+  EXPECT_TRUE(um2::isApprox(p00, quad[0]))
+  EXPECT_TRUE(um2::isApprox(p10, quad[1]))
+  EXPECT_TRUE(um2::isApprox(p01, quad[3]))
+  EXPECT_TRUE(um2::isApprox(p11, quad[2]))
+}
+
+// -------------------------------------------------------------------
+// jacobian
+// -------------------------------------------------------------------
+
+template <len_t D, typename T>
+UM2_HOSTDEV TEST_CASE(jacobian)
+{
+  // For the reference quad, the Jacobian is constant.
+  um2::Quadrilateral<D, T> quad = makeQuad<D, T>();
+  um2::Mat<D, 2, T> jac = quad.jacobian(0, 0);
+  EXPECT_NEAR((jac(0, 0)), 1, 1e-5);
+  EXPECT_NEAR((jac(1, 0)), 0, 1e-5);
+  EXPECT_NEAR((jac(0, 1)), 0, 1e-5);
+  EXPECT_NEAR((jac(1, 1)), 1, 1e-5);
+  jac = quad.jacobian(static_cast<T>(0.2), static_cast<T>(0.3));
+  EXPECT_NEAR((jac(0, 0)), 1, 1e-5);
+  EXPECT_NEAR((jac(1, 0)), 0, 1e-5);
+  EXPECT_NEAR((jac(0, 1)), 0, 1e-5);
+  EXPECT_NEAR((jac(1, 1)), 1, 1e-5);
+}
+
+// -------------------------------------------------------------------
+// edge
+// -------------------------------------------------------------------
+
+template <len_t D, typename T>
+UM2_HOSTDEV TEST_CASE(edge)
+{
+  um2::Quadrilateral<D, T> quad = makeQuad<D, T>();
+  um2::LineSegment<D, T> edge = quad.edge(0);
+  EXPECT_TRUE(um2::isApprox(edge[0], quad[0]));
+  EXPECT_TRUE(um2::isApprox(edge[1], quad[1]));
+  edge = quad.edge(1);
+  EXPECT_TRUE(um2::isApprox(edge[0], quad[1]));
+  EXPECT_TRUE(um2::isApprox(edge[1], quad[2]));
+  edge = quad.edge(2);
+  EXPECT_TRUE(um2::isApprox(edge[0], quad[2]));
+  EXPECT_TRUE(um2::isApprox(edge[1], quad[3]));
+  edge = quad.edge(3);
+  EXPECT_TRUE(um2::isApprox(edge[0], quad[3]));
+  EXPECT_TRUE(um2::isApprox(edge[1], quad[0]));
+}
+
+// -------------------------------------------------------------------
+// contains
+// -------------------------------------------------------------------
+
+template <typename T>
+UM2_HOSTDEV TEST_CASE(contains)
+{
+  um2::Quadrilateral<2, T> quad = makeQuad<2, T>();
+  um2::Point2<T> p = um2::Point2<T>(static_cast<T>(0.25), static_cast<T>(0.25));
+  EXPECT_TRUE(quad.contains(p));
+  p = um2::Point2<T>(static_cast<T>(0.5), static_cast<T>(0.25));
+  EXPECT_TRUE(quad.contains(p));
+  p = um2::Point2<T>(static_cast<T>(1.25), static_cast<T>(0.25));
+  EXPECT_FALSE(quad.contains(p));
+  p = um2::Point2<T>(static_cast<T>(0.25), static_cast<T>(-0.25));
+  EXPECT_FALSE(quad.contains(p));
+}
+
+// -------------------------------------------------------------------
+// area
+// -------------------------------------------------------------------
+
+template <len_t D, typename T>
+UM2_HOSTDEV TEST_CASE(area)
+{
+  um2::Quadrilateral<2, T> quad = makeQuad<2, T>();
+  EXPECT_NEAR(area(quad), 1, 1e-5);
+}
+// -------------------------------------------------------------------
+// centroid
+// -------------------------------------------------------------------
+
+template <len_t D, typename T>
+UM2_HOSTDEV TEST_CASE(centroid)
+{
+  um2::Quadrilateral<D, T> quad = makeQuad<D, T>();
+  um2::Point<D, T> c = centroid(quad);
+  EXPECT_NEAR(c[0], 0.5, 1e-5);
+  EXPECT_NEAR(c[1], 0.5, 1e-5);
+  quad[2] = um2::Point<D, T>(static_cast<T>(2), static_cast<T>(0.5));
+  quad[3] = um2::Point<D, T>(static_cast<T>(1), static_cast<T>(0.5));
+  c = centroid(quad);
+  EXPECT_NEAR(c[0], 1, 1e-5);
+  EXPECT_NEAR(c[1], 0.25, 1e-5);
+}
+
+// -------------------------------------------------------------------
+// boundingBox
+// -------------------------------------------------------------------
+
+template <len_t D, typename T>
+UM2_HOSTDEV TEST_CASE(boundingBox)
+{
+  um2::Quadrilateral<D, T> quad = makeQuad<D, T>();
+  um2::AABox<D, T> box = boundingBox(quad);
+  EXPECT_NEAR(box.xmin(), 0, 1e-5);
+  EXPECT_NEAR(box.ymin(), 0, 1e-5);
+  EXPECT_NEAR(box.xmax(), 1, 1e-5);
+  EXPECT_NEAR(box.ymax(), 1, 1e-5);
+}
+
+#if UM2_ENABLE_CUDA
+template <len_t D, typename T>
+MAKE_CUDA_KERNEL(interpolate, D, T);
+
+template <len_t D, typename T>
+MAKE_CUDA_KERNEL(jacobian, D, T);
+
+template <len_t D, typename T>
+MAKE_CUDA_KERNEL(edge, D, T);
+
+template <typename T>
+MAKE_CUDA_KERNEL(contains, T);
+
+template <len_t D, typename T>
+MAKE_CUDA_KERNEL(area, D, T);
+
+template <len_t D, typename T>
+MAKE_CUDA_KERNEL(centroid, D, T);
+
+template <len_t D, typename T>
+MAKE_CUDA_KERNEL(boundingBox, D, T);
+#endif // UM2_HAS_CUDA
+
+template <len_t D, typename T>
+TEST_SUITE(quadrilateral)
+{
+  TEST_HOSTDEV(interpolate, 1, 1, D, T);
+  TEST_HOSTDEV(jacobian, 1, 1, D, T);
+  TEST_HOSTDEV(edge, 1, 1, D, T);
+  if constexpr (D == 2) {
+    TEST_HOSTDEV(contains, 1, 1, T);
+  }
+  TEST_HOSTDEV(area, 1, 1, D, T);
+  //  TEST_HOSTDEV(centroid, 1, 1, D, T);
+  TEST_HOSTDEV(boundingBox, 1, 1, D, T);
+}
+
+auto main() -> int
+{
+  RUN_TESTS((quadrilateral<2, float>));
+  RUN_TESTS((quadrilateral<3, float>));
+  RUN_TESTS((quadrilateral<2, double>));
+  RUN_TESTS((quadrilateral<3, double>));
+  return 0;
+}
