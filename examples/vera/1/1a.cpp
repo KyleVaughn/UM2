fab162afb102d9293502d81c49ce7b44724ac3b1
Kyle Vaughn <kcvaughn@umich.edu>
2023-08-29 12:37:50 -0400

    WIP: material-based mesh size fields

--- examples/vera/1/1a.cpp
+++ examples/vera/1/1a.cpp
@@ -24,9 +24,15 @@ main(int argc, char * argv[]) -> int
   model.makeAssembly({0});
   model.makeCore({{0}});
   um2::gmsh::model::occ::overlaySpatialPartition(model);
-  um2::gmsh::model::mesh::setGlobalMeshSize(lc);
+//  um2::gmsh::model::mesh::setGlobalMeshSize(lc);
+
+  um2::gmsh::model::mesh::setMeshFieldFromGroups(2,
+      {"Material_Fuel", "Material_Gap", "Material_Clad", "Material_Moderator"},
+      {0.1, 0.01, 0.1, 0.2}
+  }
+
   um2::gmsh::model::mesh::generateMesh(mesh_type);
-  // um2::gmsh::fltk::run();
+  um2::gmsh::fltk::run();
   um2::gmsh::write("1a.inp");
   model.importCoarseCells("1a.inp");
   um2::exportMesh("1a.xdmf", model);
