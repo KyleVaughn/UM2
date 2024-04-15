a59f123b21e563c9fcfd08095250c0a8dfc07f2f
Kyle Vaughn <kcvaughn@umich.edu>
2024-04-15 18:42:31 -0400

    Fixed flush at exit and terminal color

--- src/um2.cpp
+++ src/um2.cpp
@@ -1,6 +1,8 @@
 #include <um2.hpp>
 #include <um2/common/logger.hpp>
 
+#include <unistd.h> // isatty
+
 namespace um2
 {
 
@@ -8,6 +10,9 @@ void
 initialize()
 {
   logger::reset();
+  if (isatty(fileno(stdout)) == 0 || isatty(fileno(stderr)) == 0) {
+    logger::colorized = false;
+  }
   logger::info("Initializing UM2");
 #if UM2_USE_GMSH
   if (gmsh::isInitialized() == 0) {
@@ -24,6 +29,9 @@ void
 finalize()
 {
   logger::info("Finalizing UM2");
+  if (fflush(stdout) != 0) {
+    logger::error("fflush(stdout) failed");
+  }
 #if UM2_USE_GMSH
   if (gmsh::isInitialized() != 0) {
     gmsh::finalize();
