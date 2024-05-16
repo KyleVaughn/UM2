a92904de0fc570c889a11576a54ed2f139637614
Kyle Vaughn <kcvaughn@umich.edu>
2024-05-16 14:05:22 -0400

    Updated install script

--- scripts/install/apt_install.sh
+++ scripts/install/apt_install.sh
@@ -9,6 +9,14 @@ sudo apt install -y \
     cmake \
     git
 
+# Optional packages
+sudo apt install -y \
+  libopenblas-dev \
+  liblapacke-dev \
+  libhdf5-dev \
+  libpugixml-dev \
+  libgmsh-dev
+
 # Development tools
 sudo apt install -y \
     clang-format \
